448f9d47a0a77f0fdf01c0d4c0f9042a882ff475
gvdoord <g.vandenOord@gmail.com>
2016-09-03 17:03:36 +0200

    Added nemo file utils and tests

--- cmorutils.py
+++ cmorutils.py
@@ -1,5 +1,8 @@
 import os
 import re
+import datetime
+
+# Creates time intervals between start and end with length delta. Last interval may be cut to match end-date.
 
 def make_time_intervals(start,end,delta):
     if(end<start):
@@ -15,6 +18,7 @@ def make_time_intervals(start,end,delta):
     result.append((istart,end))
     return result
 
+# Finds all ifs output in the given directory. If expname is given, matches according output files.
 
 def find_ifs_output(path,expname=None):
     subexpr='.*'
@@ -23,21 +27,32 @@ def find_ifs_output(path,expname=None):
     expr=re.compile('^(ICMGG|ICMSH)'+subexpr+'\+00[0-9]{4}$')
     return [os.path.join(path,f) for f in os.listdir(path) if re.match(expr,f)]
 
+# Returns the number of output time steps from the given ifs output file.
 
 def get_ifs_steps(filepath):
-    print "the file path is",filepath
     fname=os.path.basename(filepath)
     regex=re.search('\+00[0-9]{4}',fname)
     if(not regex):
-        raise Exception('unable to parse time stamp from file name',fname)
+        raise Exception('unable to parse time stamp from ifs file name',fname)
     ss=regex.group()[3:]
     return int(ss)
 
-
-
+# Finds all nemo output in the given directory. If expname is given, matches according output files.
 
 def find_nemo_output(path,expname=None):
-    subexpr=expname
-    if(not subexpr):
-        subexpr='.*'
+    subexpr='.*'
+    if(expname):
+        subexpr=expname
     expr=re.compile(subexpr+'_.*_[0-9]{8}_[0-9]{8}_.*.nc')
+    return [os.path.join(path,f) for f in os.listdir(path) if re.match(expr,f)]
+
+# Returns the start and end date corresponding to the given nemo output file.
+
+def get_nemo_interval(filepath):
+    fname=os.path.basename(filepath)
+    regex=re.findall('_[0-9]{8}',fname)
+    if(not regex or len(regex)!=2):
+        raise Exception('unable to parse dates from nemo file name',fname)
+    start=datetime.datetime.strptime(regex[0][1:],"%Y%m%d")
+    end=datetime.datetime.strptime(regex[1][1:],"%Y%m%d")
+    return (start,end)
--- test/cmorutilstests.py
+++ test/cmorutilstests.py
@@ -3,37 +3,69 @@ import unittest
 from datetime import datetime,timedelta
 from dateutil.relativedelta import relativedelta
 from nose.tools import eq_,ok_
-from cmorutils import make_time_intervals,find_ifs_output,get_ifs_steps
+from cmorutils import make_time_intervals,find_ifs_output,get_ifs_steps,find_nemo_output,get_nemo_interval
 
 logging.basicConfig(level=logging.DEBUG)
 
 class TestTimeIntervals(unittest.TestCase):
 
-    def test1(self):
+    def test_intervals_30days(self):
         t1=datetime(1999,12,31,23,45,20)
         t2=datetime(2003,8,29,12,0,0)
         intervals=make_time_intervals(t1,t2,timedelta(days=30))
         eq_(intervals[-1][1],t2)
 
 
-    def test2(self):
+    def test_intervals_month(self):
         t1=datetime(2000,1,1,23,45,20)
         t2=datetime(2003,8,29,12,0,0)
         intervals=make_time_intervals(t1,t2,relativedelta(months=+1))
         eq_(intervals[-1][0],datetime(2003,8,1,23,45,20))
 
-    def test3(self):
-        ofiles=find_ifs_output('./test_data')
+
+    def test_find_ifs_output(self):
+        ofiles=find_ifs_output('./test_data/ifs_dummy')
         eq_(len(ofiles),2)
-        ok_('./test_data/ICMGGGbla+003456' in ofiles)
-        ok_('./test_data/ICMSHok+004321' in ofiles)
+        ok_('./test_data/ifs_dummy/ICMGGGbla+003456' in ofiles)
+        ok_('./test_data/ifs_dummy/ICMSHok+004321' in ofiles)
 
-    def test4(self):
-        ofiles=find_ifs_output('./test_data','ok')
+
+    def test_find_ifs_exp_output(self):
+        ofiles=find_ifs_output('./test_data/ifs_dummy','ok')
         eq_(len(ofiles),1)
-        ok_('./test_data/ICMSHok+004321' in ofiles)
+        ok_('./test_data/ifs_dummy/ICMSHok+004321' in ofiles)
 
-    def test4(self):
-        ofiles=find_ifs_output('./test_data','Gbla')
+
+    def test_ifs_output_timestamps(self):
+        ofiles=find_ifs_output('./test_data/ifs_dummy','Gbla')
         eq_(len(ofiles),1)
         eq_(get_ifs_steps(ofiles[0]),3456)
+
+
+    def test_find_nemo_output(self):
+        ofiles=find_nemo_output('./test_data/nemo_dummy')
+        eq_(len(ofiles),2)
+        ok_('./test_data/nemo_dummy/exp_1m_19991231_20000505_gridT.nc' in ofiles)
+        ok_('./test_data/nemo_dummy/exp_1d_19991221_20000101_icemod.nc' in ofiles)
+
+
+    def test_find_nemo_exp_output(self):
+        ofiles=find_nemo_output('./test_data/nemo_dummy','exp')
+        eq_(len(ofiles),2)
+        ok_('./test_data/nemo_dummy/exp_1m_19991231_20000505_gridT.nc' in ofiles)
+        ok_('./test_data/nemo_dummy/exp_1d_19991221_20000101_icemod.nc' in ofiles)
+
+
+    def test_nemo_bad_output(self):
+        ofiles=find_nemo_output('./test_data/nemo_dummy','bad')
+        eq_(len(ofiles),0)
+
+
+    def test_nemo_output_timestamps(self):
+        ofiles=find_nemo_output('./test_data/nemo_dummy','exp')
+        dates=[get_nemo_interval(f) for f in ofiles]
+        print dates
+        starts=sorted([t[0] for t in dates])
+        eq_([datetime(1999,12,21),datetime(1999,12,31)],starts)
+        ends=sorted([t[1] for t in dates])
+        eq_([datetime(2000,1,1),datetime(2000,5,5)],ends)
