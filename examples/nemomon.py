3da8952a771d7e9d3b6be0a99c0c67530864eefc
gvdoord <g.vandenOord@gmail.com>
2016-11-03 17:30:46 +0100

    Added example for nemo variables

---
+++ examples/nemomon.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import ece2cmor
+import namloader
+import optparse
+import datetime
+from dateutil.relativedelta import relativedelta
+
+# This example script performs cmorization of one month of data, starting at \
+# januari 1st 1990. It requires an output directory, a configuration json-file \
+# and an experiment name/prefix to determine the output data files and configure \
+# cmor3 correctly. The processed variables are listed in the "variables" dictionary
+
+variables = {"Omon" : ["sos","tos"]}
+startdate = datetime.date(1990,1,1)
+interval = relativedelta(months=1)
+
+def main(args):
+
+    parser = optparse.OptionParser()
+    parser.add_option("-d","--dir" ,dest = "dir" ,help = "NEMO output directory")
+    parser.add_option("-c","--conf",dest = "conf",help = "CMOR3 meta data json file path",metavar = "FILE")
+    parser.add_option("-e","--exp" ,dest = "exp" ,help = "Experiment name (prefix)")
+    (opt,args) = parser.parse_args()
+    odir = os.path.abspath(opt.dir)
+    if(not os.path.isdir(odir)): raise Exception("Nonexistent output directory given:",odir)
+
+    # Initialize ece2cmor with metadata and experiment prefix:
+    ece2cmor.initialize(opt.conf,opt.exp)
+
+    # Set directory and time interval for cmorization step:
+    ece2cmor.nemodir = odir
+    ece2cmor.startdate = startdate
+    ece2cmor.interval = interval
+
+    # Load the variables as task targets:
+    namloader.load_targets(variables)
+
+    # Execute the cmorizatioon:
+    ece2cmor.perform_nemo_tasks()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
