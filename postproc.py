308d50c46744181a46c89692694997417bac66ef
Oord van den <G.vandenOord@gmail.com>
2016-12-07 09:10:36 +0000

    Added post-processing functionality of full task list to postproc.py

--- postproc.py
+++ postproc.py
@@ -3,6 +3,56 @@ import cdoapi
 import cmor_source
 import cmor_target
 
+task_threads = 1
+cdo_threads = 4
+apply_cdo = True
+
+# Post-processes a list of tasks
+def post_process(tasks,path):
+    comdict = {}
+    commbuf = {}
+    for task in tasks:
+        command = create_command(tasks)
+        commstr = command.create_command()
+        if(commstr not in commbuf):
+            commbuf[commstr] = command
+        else:
+            command = commbuf[commstr]
+        if(command in comdict):
+            comdict[command].append(task)
+        else:
+            comdict[command] = [task]
+    for (comm,tasklist) in comdict:
+        if(not validate_tasklist(tasklist)):
+            comdict.pop(comm)
+    if(task_threads <= 2):
+        for (comm,tasklist) in comdict:
+            apply_command((comm,tasklist),path)
+    else:
+        q = Queue.Queue()
+        for i in range(numthreads):
+            worker = threading.Thread(target = apply_command,args = (q,basepath))
+            worker.setDaemon(True)
+            worker.start()
+        for (comm,tasklist) in comdict:
+            q.put((comm,tasklist))
+        q.join()
+
+# Checks whether the task grouping makes sense: only tasks for the same variable and frequency can be safely grouped.
+def validate_tasklist(tasks):
+    srcset = set(map(lambda t:t.source.get_grib_code().var_id,tasks))
+    if(len(srcset) != 1):
+        log.error("Multiple grib codes joined to single cdo command: %s" % str(srcset))
+        return False
+    tgtset = set(map(lambda t:t.target.variable,tasks))
+    if(len(tgtset) != 1):
+        log.error("Multiple target variables joined to single cdo command: %s" % str(tgtset))
+        return False
+    freqset = set(map(lambda t:t.target.frequency,tasks))
+    if(len(freqset) != 1):
+        log.error("Multiple target variables joined to single cdo command: %s" % str(freqset))
+        return False
+    return True
 
 # Creates a cdo postprocessing command for the given IFS task.
 def create_command(task):
@@ -23,6 +73,18 @@ def create_command(task):
     add_level_operators(result,task)
     return result
 
+# Executes the command (first item of tup), and replaces the path attribute for all tasks in the tasklist (2nd item of tup)
+# to the output of cdo. This path is constructed from the basepath and the first task.
+def apply_command(tup,basepath):
+    command,tasklist = tup[0],tup[1]
+    if(not tasklist):
+        log.warning("Encountered empty task list for post-processing command %s" % command.create_command())
+    ifile = getattr(tasklist[0],"path")
+    ofile = "_".join(basepath,tasklist[0].target.variable,tasklist[0].target.table) + ".nc"
+    if(apply_cdo):
+        command.apply(ifile,ofile,cdo_threads)
+    for task in tasklist:
+        setattr(task,"path",ofile)
 
 # Translates the cmor time post-processing operation to a cdo command-line option
 def add_time_operators(cdo,freq,operators):
