fa8f1d734b67da94597a44afa81e7fd95d811666
gvdoord <g.vandenOord@gmail.com>
2017-02-20 10:28:16 +0100

    Text formatting cleanup in ece2cmor.py

--- ece2cmor.py
+++ ece2cmor.py
@@ -24,32 +24,32 @@ startdate = None
 interval = None
 
 # Initialization function, must be called before starting
-def initialize(conf_path,exp_name_ = None):
+def initialize(conf_path = os.path.join(os.path.dirname(__file__),"test","test_data","cmor3_metadata.json"),exp_name_ = None):
     global exp_name
     global targets
     exp_name = exp_name_
-    conf_path_=conf_path
+    conf_path_ = conf_path
     cmor.setup(table_dir)
     cmor.dataset_json(conf_path)
-    targets=cmor_target.create_targets(table_dir,prefix)
+    targets = cmor_target.create_targets(table_dir,prefix)
 
 # Closes cmor
 def finalize():
     global tasks
     global targets
     cmor.close()
-    targets=[]
-    tasks=[]
+    targets = []
+    tasks = []
 
 # Returns one or more cmor targets for task creation.
 def get_cmor_target(var_id,tab_id=None):
-    if(tab_id==None):
-        return [t for t in targets if t.variable==var_id]
+    if(tab_id == None):
+        return [t for t in targets if t.variable == var_id]
     else:
-        results=[t for t in targets if t.variable==var_id and t.table==tab_id]
-        if(len(results)==1):
+        results = [t for t in targets if t.variable == var_id and t.table == tab_id]
+        if(len(results) == 1):
             return results[0]
-        elif(len(results)==0):
+        elif(len(results) == 0):
             return None
         else:
             log.error("Table validation error: multiple variables with name %s found in table %s" % (var_id,tab_id))
@@ -70,8 +70,8 @@ def add_task(tsk):
 
 # Performs an IFS cmorization processing:
 def perform_ifs_tasks(postprocmode = postproc.recreate,tempdir = None,taskthreads = 4,cdothreads = 4,cleanup = True,outputfreq = 3,maxsizegb = float("inf")):
-    ifs_tasks=[t for t in tasks if isinstance(t.source,cmor_source.ifs_source)]
-    tableroot=os.path.join(table_dir,prefix)
+    ifs_tasks = [t for t in tasks if isinstance(t.source,cmor_source.ifs_source)]
+    tableroot = os.path.join(table_dir,prefix)
     # TODO: Add support for reference date other that startdate
     ifs2cmor.initialize(ifsdir,exp_name,tableroot,startdate,interval,startdate,outputfreq = outputfreq,tempdir=tempdir,maxsizegb = maxsizegb)
     postproc.postproc_mode = postprocmode
@@ -85,7 +85,7 @@ def perform_ifs_tasks(postprocmode = postproc.recreate,tempdir = None,taskthread
 
 # Performs a NEMO cmorization processing:
 def perform_nemo_tasks():
-    nemo_tasks=[t for t in tasks if isinstance(t.source,cmor_source.nemo_source)]
-    tableroot=os.path.join(table_dir,prefix)
+    nemo_tasks = [t for t in tasks if isinstance(t.source,cmor_source.nemo_source)]
+    tableroot = os.path.join(table_dir,prefix)
     nemo2cmor.initialize(nemodir,exp_name,tableroot,startdate,interval)
     nemo2cmor.execute(nemo_tasks)
