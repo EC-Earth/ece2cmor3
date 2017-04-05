a6c5ba10057b8d5925bc67f50988ca8df245e77a
Oord van den <G.vandenOord@gmail.com>
2017-04-05 10:20:38 +0000

    Added data request csv files

---
+++ resources/data_request/makecsv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for MIP in {CMIP6,PRIMAVERA}; do
+mkdir -p ${MIP}
+    for f in ../cmip6-cmor-tables/Tables/${MIP}_*.json; do
+        fname=$(basename $f)
+        fname=${fname%.json}
+        table=${fname#${MIP}_}
+        ofile=${MIP}/${table}.csv
+        echo "Converting data for ${table} to ${ofile}..."
+        xlsx2csv -n ${table} -i PRIMAVERA_Data_Request.xlsx > ${ofile}
+        if [ ! -s ${ofile} ]; then
+            rm -f ${ofile}
+        fi
+    done
+done
