ebf21dad2dcb57c2bd3c7d84c88a40cc59e767ca
gvdoord <g.vandenOord@gmail.com>
2018-03-29 11:03:21 +0200

    Added test for store_var

--- test/postproc/operator_test.py
+++ test/postproc/operator_test.py
@@ -9,10 +9,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 # Test utility class summing last 10 values
-class pp_sum_operator(operator.operator_base):
+class sum_operator(operator.operator_base):
 
     def __init__(self, interval=10):
-        super(pp_sum_operator, self).__init__()
+        super(sum_operator, self).__init__()
         self.counter = 0
         self.interval = interval
 
@@ -28,13 +28,13 @@ class pp_sum_operator(operator.operator_base):
         self.values, self.counter = None, 0
 
     def cache_is_full(self):
-        return self.counter == self.interval
+        return super(sum_operator, self).cache_is_full() and self.counter == self.interval
 
 
 # Test utility class multiplying single value
-class pp_mult_operator(operator.operator_base):
+class multiply_operator(operator.operator_base):
     def __init__(self, factor=0.5):
-        super(pp_mult_operator, self).__init__()
+        super(multiply_operator, self).__init__()
         self.factor = factor
 
     def fill_cache(self, msg):
@@ -58,57 +58,86 @@ class post_proc_operator_test(unittest.TestCase):
 
     @staticmethod
     def test_start_with_empty_cache():
-        operator_1 = pp_sum_operator()
-        operator_2 = pp_mult_operator()
+        operator_1 = sum_operator()
+        operator_2 = multiply_operator()
         ok_(operator_1.cache_is_empty)
         ok_(operator_2.cache_is_empty)
 
     @staticmethod
     def test_receive_msg():
-        operator = pp_sum_operator()
-        operator.cached_properties = [message.variable_key, message.leveltype_key, message.levellist_key]
+        op = sum_operator()
+        op.cached_properties = [message.variable_key, message.leveltype_key, message.levellist_key]
         time = datetime(1990, 1, 1, 6, 0, 0)
-        operator.receive_msg(make_msg(130, time, 90, values=285.87))
-        eq_(operator.property_cache,
+        op.receive_msg(make_msg(130, time, 90, values=285.87))
+        eq_(op.property_cache,
             {message.variable_key: cmor_source.ifs_source(cmor_source.grib_code(130, 128)),
              message.leveltype_key: grib_file.hybrid_level_code,
              message.levellist_key: [90]})
-        operator.receive_msg(make_msg(130, time, 90, values=214.13))
-        eq_(operator.values, 500.)
-        operator.receive_msg(make_msg(131, time, 90, values=-6.5))
-        eq_(operator.values, 500.)
+        op.receive_msg(make_msg(130, time, 90, values=214.13))
+        eq_(op.values, 500.)
+        op.receive_msg(make_msg(131, time, 90, values=-6.5))
+        eq_(op.values, 500.)
 
     @staticmethod
-    def test_create_msg():
-        operator = pp_sum_operator()
-        operator.cached_properties = [message.variable_key,
-                                      message.leveltype_key,
-                                      message.levellist_key,
-                                      message.datetime_key,
-                                      message.timebounds_key,
-                                      message.resolution_key]
+    def test_fill_cache():
+        op = sum_operator()
+        op.cached_properties = [message.variable_key,
+                                message.leveltype_key,
+                                message.levellist_key,
+                                message.datetime_key,
+                                message.timebounds_key,
+                                message.resolution_key]
         time = datetime(1990, 1, 1, 6, 0, 0)
-        operator.receive_msg(make_msg(130, time, 90, values=285.87))
-        operator.receive_msg(make_msg(130, time, 90, values=214.13))
-        msg = operator.create_msg()
+        value = 0.
+        tot_value = 0.
+        for i in range(9):
+            value += i * i
+            tot_value += value
+            op.receive_msg(make_msg(130, time, 90, values=value))
+        ok_(not op.cache_is_full())
+        value -= 10.
+        tot_value += value
+        op.receive_msg(make_msg(130, time, 90, values=value))
+        ok_(op.cache_is_full())
+        msg = op.create_msg()
         eq_(msg.get_variable(), cmor_source.ifs_source(cmor_source.grib_code(130, 128)))
         eq_(msg.get_levels(), [90])
-        eq_(msg.get_values(), 500.)
+        eq_(msg.get_values(), tot_value)
 
     @staticmethod
     def test_transfer_msg():
-        operator_1, operator_2, operator_3 = pp_mult_operator(factor=2.0), pp_sum_operator(
-            interval=8), pp_mult_operator(factor=0.3)
-        operator_2.targets.append(operator_3)
-        operator_1.targets.append(operator_2)
+        op1, op2, op3 = multiply_operator(factor=2.0), sum_operator(interval=8), multiply_operator(factor=0.3)
+        op2.targets.append(op3)
+        op1.targets.append(op2)
         result = 0
         time = datetime(1990, 1, 1, 6, 0, 0)
         for i in range(8):
             val = 273.5 + i
             result += 2.0 * val
-            operator_1.receive_msg(make_msg(130, time, 90, values=val))
+            op1.receive_msg(make_msg(130, time, 90, values=val))
             if i < 7:
-                ok_(operator_3.cache_is_empty())
-        ok_(operator_3.cache_is_full())
-        final_msg = operator_3.create_msg()
+                ok_(op3.cache_is_empty())
+        ok_(op3.cache_is_full())
+        final_msg = op3.create_msg()
         eq_(final_msg.get_values(), 0.3 * result)
+
+    @staticmethod
+    def test_receive_store_var():
+        op = sum_operator()
+        op.cached_properties = [message.variable_key,
+                                message.leveltype_key,
+                                message.levellist_key,
+                                message.datetime_key,
+                                message.timebounds_key,
+                                message.resolution_key]
+        op.store_var_key = (134, 128, grib_file.hybrid_level_code, 91)
+        time = datetime(1990, 1, 1, 6, 0, 0)
+        value = 0.
+        tot_value = 0.
+        for i in range(10):
+            value += i * i
+            tot_value += value
+            op.receive_msg(make_msg(130, time, 90, values=value))
+        ok_(not op.cache_is_full())
+        op.receive_store_var(make_msg(134, time, 91, values=tot_value))
+        ok_(op.cache_is_full())
