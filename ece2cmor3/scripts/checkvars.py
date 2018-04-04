1fc2f3106d418d68a94a476db4cc4a0077367642
treerink <tjreerink@gmail.com>
2018-04-04 15:42:37 +0200

    Interchanging the writing of the ascii and xmlx files, because the ascii writing is more vulnarable for strange characters which can be added in the comment column be others.

--- ece2cmor3/scripts/checkvars.py
+++ ece2cmor3/scripts/checkvars.py
@@ -119,15 +119,16 @@ def main():
             if(output_dir != ''): os.makedirs(output_dir)
         write_varlist(loadedtargets,args.output + ".available.json")
         if(args.verbose):
+            write_varlist_excel(loadedtargets           ,args.output + ".available.xlsx")
+            write_varlist_excel(ignoredtargets          ,args.output + ".ignored.xlsx")
+            write_varlist_excel(identifiedmissingtargets,args.output + ".identifiedmissing.xlsx")
+            write_varlist_excel(missingtargets          ,args.output + ".missing.xlsx")
+
             write_varlist_ascii(loadedtargets           ,args.output + ".available.txt")
             write_varlist_ascii(ignoredtargets          ,args.output + ".ignored.txt")
             write_varlist_ascii(identifiedmissingtargets,args.output + ".identifiedmissing.txt")
             write_varlist_ascii(missingtargets          ,args.output + ".missing.txt")
 
-            write_varlist_excel(loadedtargets           ,args.output + ".available.xlsx")
-            write_varlist_excel(ignoredtargets          ,args.output + ".ignored.xlsx")
-            write_varlist_excel(identifiedmissingtargets,args.output + ".identifiedmissing.xlsx")
-            write_varlist_excel(missingtargets          ,args.output + ".missing.xlsx")
 
     # Finishing up
     ece2cmorlib.finalize_without_cmor()
