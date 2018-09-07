2229bbc4ec312ce68f9331049955d5a5b6214f83
treerink <tjreerink@gmail.com>
2018-09-07 14:37:30 +0200

    Add MIP overview info #197.

--- ece2cmor3/scripts/mip-experiment-list.py
+++ ece2cmor3/scripts/mip-experiment-list.py
@@ -11,14 +11,22 @@
 from dreqPy import dreq
 dq = dreq.loadDreq()
 
+mip_counter        = 0
+experiment_counter = 0
+
 mip_list_file= open( 'mip-experiment-list.txt', 'w' )
 
 # Loop over the MIPs:
 for mip in dq.coll['mip'].items:
+  mip_counter = mip_counter + 1
+  print '{:3} {:15} {}'.format(mip_counter, mip.label, mip)
   # Loop over the MIP experiments:
   for u in dq.inx.iref_by_sect[mip.uid].a['experiment']:
     ex = dq.inx.uid[u]
     mip_list_file.write( '{:20} {:20} {:30} {:3} {}'.format(mip.label, ex.mip, ex.label, ex.tier[0], ex.title) + '\n')
    #print '{:20} {:20} {:30} {:3} {}'.format(mip.label, ex.mip, ex.label, ex.tier[0], ex.title)
-
+    experiment_counter = experiment_counter + 1
+    
 mip_list_file.close()
+
+print 'Number of MIPs is: {:3} and the number of MIP experiments is: {}'.format(mip_counter, experiment_counter)
