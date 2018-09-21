2be43cd7c24c272aec90f42c81b60c97d3195ef4
Thomas Reerink <thomas.reerink.knmi@gmail.com>
2018-09-21 10:16:20 +0200

    Add adhoc rule for removing the lines with sfdsi_2 from the opa file_def files.

--- ece2cmor3/scripts/generate-ec-earth-namelists-for-all-MIP-experiments.py
+++ ece2cmor3/scripts/generate-ec-earth-namelists-for-all-MIP-experiments.py
@@ -38,6 +38,7 @@ for mip in dq.coll['mip'].items:
     command_2 = 'rm -rf ec-earth-cmip6-nemo-namelists/' + mip.label + '/cmip6-experiment-m=*/file_def-compact'
     command_3 = 'rm -f  ec-earth-cmip6-nemo-namelists/' + mip.label + '/cmip6-experiment-m=*/cmip6-file_def_nemo.xml'
     command_4 = "sed -i -e 's/True\" field_ref=\"toce_pot\"/False\" field_ref=\"toce_pot\"/' ec-earth-cmip6-nemo-namelists/" + mip.label + '/cmip6-experiment-m=' + mip.label + '-e=' + ex.label + '-t=' + str(ex.tier[0]) + '-p=1/file_def_nemo-opa.xml'
+    command_5 = "sed -i -e '/sfdsi_2/d' ec-earth-cmip6-nemo-namelists/" + mip.label + '/cmip6-experiment-m=' + mip.label + '-e=' + ex.label + '-t=' + str(ex.tier[0]) + '-p=1/file_def_nemo-opa.xml'
   ##equivalent:  sed -i -e 's/True\" field_ref=\"toce_pot\"/False\" field_ref=\"toce_pot\"/' cmip6-experiment-m=CMIP-e=piControl-t=1-p=1/file_def_nemo-opa.xml
    #print print '{}'.format(command)
     if mip.label in ec_earth_mips: 
@@ -47,6 +48,7 @@ for mip in dq.coll['mip'].items:
           os.system(command_2)
           os.system(command_3)
           os.system(command_4) # Just set the toce fields false again because we still face troubles with them
+          os.system(command_5) # Delete the line with sfdsi_2 from the file_def_nemo-opa.xml files
           experiment_counter = experiment_counter + 1
        else:
           print ' Tier {:2} experiments are not included: Skipping: {}'.format(ex.tier[0], command)
