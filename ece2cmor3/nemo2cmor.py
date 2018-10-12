4eb5e83d755a115707e966d352d33d56a486aec7
goord <G.vandenOord@gmail.com>
2018-10-12 13:19:39 +0000

    Removed 'positive' fix for nemo cmorization

--- ece2cmor3/nemo2cmor.py
+++ ece2cmor3/nemo2cmor.py
@@ -186,12 +186,8 @@ def create_cmor_variable(task, dataset, axes):
     unit = getattr(ncvar, "units", None)
     if (not unit) or hasattr(task, cmor_task.conversion_key):  # Explicit unit conversion
         unit = getattr(task.target, "units")
-    if hasattr(task.target, "positive") and len(task.target.positive) != 0:
-        return cmor.variable(table_entry=str(task.target.variable), units=str(unit), axis_ids=axes,
-                             original_name=str(srcvar), positive="down")
-    else:
-        return cmor.variable(table_entry=str(task.target.variable), units=str(unit), axis_ids=axes,
-                             original_name=str(srcvar))
+    return cmor.variable(table_entry=str(task.target.variable), units=str(unit), axis_ids=axes,
+                         original_name=str(srcvar))
 
 
 # Creates all depth axes for the given table from the given files
