4cb1dfd303bfe6d51db56f38df1b07eafbb5e7e4
Thomas Reerink <thomas.reerink.knmi@gmail.com>
2018-10-16 11:40:11 +0200

    Set back default case: CMIP, DCPP

--- ece2cmor3/scripts/generate-ec-earth-namelists-for-all-MIP-experiments.py
+++ ece2cmor3/scripts/generate-ec-earth-namelists-for-all-MIP-experiments.py
@@ -17,8 +17,8 @@ dq = dreq.loadDreq()
 # Specify in the list below which tier experiments should be included. For 
 # instance [1,2] means tier 1 and tier 2 experiments are included:
 experiment_tiers_included = [1]
-ec_earth_mips = ['CMIP'] # for basic test
-#ec_earth_mips = ['CMIP', 'DCPP']
+#ec_earth_mips = ['CMIP'] # for basic test
+ec_earth_mips = ['CMIP', 'DCPP']
 #ec_earth_mips = ['CMIP', 'AerChemMIP', 'C4MIP',          'DAMIP', 'DCPP',                              'HighResMIP', 'ISMIP6', 'LS3MIP', 'LUMIP',         'PAMIP', 'PMIP', 'RFMIP', 'ScenarioMIP', 'VolMIP', 'CORDEX', 'DynVar', 'SIMIP', 'VIACSAB'] # All 18 EC-Earth MIPs
 #ec_earth_mips = ['CMIP', 'AerChemMIP', 'C4MIP', 'CFMIP', 'DAMIP', 'DCPP', 'FAFMIP', 'GeoMIP', 'GMMIP', 'HighResMIP', 'ISMIP6', 'LS3MIP', 'LUMIP', 'OMIP', 'PAMIP', 'PMIP', 'RFMIP', 'ScenarioMIP', 'VolMIP', 'CORDEX', 'DynVar', 'SIMIP', 'VIACSAB'] # All 23 CMIP6 MIPs
 experiment_counter = 0
