37868ee8b2e30e677cdc218ae827bee53587f7cc
Thomas Reerink <thomas.reerink.knmi@gmail.com>
2018-10-16 21:31:44 +0200

    Write IFS Volume estimates to a file #180.

--- ece2cmor3/scripts/generate-ec-earth-namelists.sh
+++ ece2cmor3/scripts/generate-ec-earth-namelists.sh
@@ -82,6 +82,7 @@ if [ "$#" -eq 4 ] || [ "$#" -eq 5 ]; then
 
   mkdir -p ${path_of_created_output_control_files}/file_def-compact
   mv -f ppt0000000000 pptdddddd0*00 ${path_of_created_output_control_files}
+  mv -f volume-estimate.txt ${path_of_created_output_control_files}
 
   # Creating the file_def files for XIOS NEMO input:.
   ./drq2file_def-nemo.py --vars cmip6-data-request/cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}/cmvme_${mip_label}_${experiment}_${tier}_${priority}.xlsx
