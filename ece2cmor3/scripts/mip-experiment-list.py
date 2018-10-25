c317bc768eec6f05568290751ae65232dbccccdc
treerink <thomas.reerink.knmi@gmail.com>
2018-10-25 22:39:30 +0200

    Clean messaging and clean content of created file.

--- ece2cmor3/scripts/mip-experiment-list.py
+++ ece2cmor3/scripts/mip-experiment-list.py
@@ -17,16 +17,20 @@ dq = dreq.loadDreq()
 mip_counter        = 0
 experiment_counter = 0
 
-mip_list_file= open( 'mip-experiment-list.txt', 'w' )
+mip_list_file_name = 'mip-experiment-list.txt'
+mip_list_file = open(mip_list_file_name, 'w' )
 
 # Loop over the MIPs:
 for mip in dq.coll['mip'].items:
   mip_counter = mip_counter + 1
-  print '{:3} {:15} {}'.format(mip_counter, mip.label, mip)
+ #print '{:3} {:15} {}'.format(mip_counter, mip.label, mip)
+  print '{:3} {:15} {}'.format(mip_counter, mip.label, str(mip).replace('Item <1.1 Model Intercomparison Project>: [', '').replace(']', '').replace(mip.label, '').replace(' - set of', 'DECK - set of'))
   # Loop over the MIP experiments:
   for u in dq.inx.iref_by_sect[mip.uid].a['experiment']:
     ex = dq.inx.uid[u]
-    mip_list_file.write( '{:20} {:20} {:30} tier-{:1} {}'.format(mip.label, ex.mip, ex.label, ex.tier[0], ex.title) + '\n')
+    if mip.label != ex.mip:
+     print ' The mip.label and ex.mip are different: {:15} versus {:15}'.format(mip.label, ex.mip)
+    mip_list_file.write( '{:12} {:28} tier-{:1}  {}'.format(ex.mip, ex.label, ex.tier[0], ex.title) + '\n')
    #print '{:20} {:20} {:30} {:3} {}'.format(mip.label, ex.mip, ex.label, ex.tier[0], ex.title)
     experiment_counter = experiment_counter + 1
     
@@ -34,4 +38,4 @@ mip_list_file.close()
 
 print 'Number of MIPs is: {:3} and the number of MIP experiments is: {}'.format(mip_counter, experiment_counter)
 
-print '\nThe file  mip-experiment-list.txt  has been created.\n'
+print '\nThe file ', mip_list_file_name, ' has been created.\n'
