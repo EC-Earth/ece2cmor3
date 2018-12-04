e5acc18ac50be2c72f4aad8842ded104a7d0363e
treerink <thomas.reerink.knmi@gmail.com>
2018-12-04 16:44:23 +0100

    Add the generation of LPJ-GUESS instruction (control output) files to genecec for all MIPs #330.

--- ece2cmor3/scripts/generate-ec-earth-namelists.sh
+++ ece2cmor3/scripts/generate-ec-earth-namelists.sh
@@ -105,8 +105,9 @@ if [ "$#" -eq 4 ] || [ "$#" -eq 5 ]; then
   # Estimating the Volume of the TM5 output:
   ./estimate-tm5-volume.py --vars cmip6-data-request/cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}/cmvme_${mip_label}_${experiment}_${tier}_${priority}.xlsx
 
-  # Estimating the Volume of the LPJ-GUESS output:
-  ./estimate-lpj-guess-volume.py --vars cmip6-data-request/cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}/cmvme_${mip_label}_${experiment}_${tier}_${priority}.xlsx
+  # Creating the instruction files for LPJ-GUESS and estimating the Volume of the LPJ-GUESS output:
+  ./drq2ins.py --vars cmip6-data-request/cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}/cmvme_${mip_label}_${experiment}_${tier}_${priority}.xlsx
+  mv -f ./lpjg_cmip6_output.ins                                     ${path_of_created_output_control_files}
 
   cat volume-estimate-ifs.txt volume-estimate-nemo.txt volume-estimate-tm5.txt volume-estimate-lpj-guess.txt > ${path_of_created_output_control_files}/volume-estimate-${mip_label}-${experiment}.txt
   rm -f volume-estimate-ifs.txt volume-estimate-nemo.txt volume-estimate-tm5.txt volume-estimate-lpj-guess.txt
