ca6e0b9e75b2edeab470ceb07b0a4120b109e8f7
treerink <thomas.reerink.knmi@gmail.com>
2018-12-12 12:13:06 +0100

    Apply drq arguments in standard order #323.

--- ece2cmor3/scripts/genecec-per-mip-experiment.sh
+++ ece2cmor3/scripts/genecec-per-mip-experiment.sh
@@ -99,11 +99,11 @@ if [ "$#" -eq 4 ] || [ "$#" -eq 5 ]; then
   echo ' '$0 "$@"
   echo
   echo 'First, the CMIP6 data request is applied by:'
-  echo ' ' drq -m ${mip} -t ${tier} -p ${priority} -e ${experiment} --xls --xlsDir cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}
+  echo ' ' drq -m ${mip} -e ${experiment} -t ${tier} -p ${priority} --xls --xlsDir cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}
   echo
 
-  mkdir -p  ${ece2cmor_root_directory}/ece2cmor3/scripts/cmip6-data-request/; cd ${ece2cmor_root_directory}/ece2cmor3/scripts/cmip6-data-request/;
-  drq -m ${mip} -t ${tier} -p ${priority} -e ${experiment} --xls --xlsDir cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}
+  mkdir -p ${ece2cmor_root_directory}/ece2cmor3/scripts/cmip6-data-request/; cd ${ece2cmor_root_directory}/ece2cmor3/scripts/cmip6-data-request/;
+  drq -m ${mip} -e ${experiment} -t ${tier} -p ${priority} --xls --xlsDir cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}
   cd ${ece2cmor_root_directory}/ece2cmor3/scripts/
   # Note that the *TOTAL* selection below has the risk that more than one file is selected (causing a crash) which only could happen if externally files are added in this directory:
 
