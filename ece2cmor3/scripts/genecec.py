098fdd591ce6c3d5b6033dd774130efa33639fac
treerink <thomas.reerink.knmi@gmail.com>
2018-12-12 12:16:04 +0100

    Intermediate step: separate the "priority" and "omit-setup" argument. Also adjust skip message format #323.

--- ece2cmor3/scripts/genecec.py
+++ ece2cmor3/scripts/genecec.py
@@ -28,7 +28,7 @@ experiment_tiers_included = [1]
 ec_earth_mips  = ['CMIP', 'AerChemMIP', 'CDRMIP', 'C4MIP',                   'DCPP',                              'HighResMIP', 'ISMIP6', 'LS3MIP', 'LUMIP', 'OMIP', 'PAMIP', 'PMIP', 'RFMIP', 'ScenarioMIP', 'VolMIP', 'CORDEX', 'DynVar', 'SIMIP', 'VIACSAB'] # All 19 EC-Earth MIPs
 #ec_earth_mips = ['CMIP', 'AerChemMIP', 'CDRMIP', 'C4MIP', 'CFMIP', 'DAMIP', 'DCPP', 'FAFMIP', 'GeoMIP', 'GMMIP', 'HighResMIP', 'ISMIP6', 'LS3MIP', 'LUMIP', 'OMIP', 'PAMIP', 'PMIP', 'RFMIP', 'ScenarioMIP', 'VolMIP', 'CORDEX', 'DynVar', 'SIMIP', 'VIACSAB'] # All 24 CMIP6 MIPs
 #ec_earth_mips = ['CMIP', 'DCPP']
-#ec_earth_mips = ['CMIP'] # for basic test
+ec_earth_mips = ['CMIP'] # for basic test
 experiment_counter = 0
 
 
@@ -44,9 +44,9 @@ for mip in dq.coll['mip'].items:
      print 'Skipping this esm experiment ' + ex.label + ' because its CMIP6 data request fails so far.\n'
     else:
      if experiment_counter == 0:
-       command = './generate-ec-earth-namelists.sh ' + mip.label + ' ' + ex.label + ' ' + str(ex.tier[0]) + ' 1'
+       command = './generate-ec-earth-namelists.sh ' + mip.label + ' ' + ex.label + ' ' + str(ex.tier[0]) + ' 1 ' + ' '
      else:
-       command = './generate-ec-earth-namelists.sh ' + mip.label + ' ' + ex.label + ' ' + str(ex.tier[0]) + ' 1 omit-setup'
+       command = './generate-ec-earth-namelists.sh ' + mip.label + ' ' + ex.label + ' ' + str(ex.tier[0]) + ' 1 ' + ' omit-setup'
      command_2 = 'rm -rf cmip6-output-control-files/' + mip.label + '/cmip6-experiment-*/file_def-compact'
      command_3 = 'rm -f  cmip6-output-control-files/' + mip.label + '/cmip6-experiment-*/cmip6-file_def_nemo.xml'
      command_4 = "sed -i -e 's/True\" field_ref=\"toce_pot\"/False\" field_ref=\"toce_pot\"/' cmip6-output-control-files/" + mip.label + '/cmip6-experiment-' + mip.label + '-' + ex.label + '/file_def_nemo-opa.xml'
@@ -64,9 +64,9 @@ for mip in dq.coll['mip'].items:
           #os.system(command_5) # Delete the line with sfdsi_2 from the file_def_nemo-opa.xml files
            experiment_counter = experiment_counter + 1
         else:
-           print ' Tier {:2} experiments are not included: Skipping: {}'.format(ex.tier[0], command)
+           print ' Tier {} experiments are not included: Skipping: {}'.format(ex.tier[0], command)
      else:
-        print ' EC-Earth3 does not participate in {:2}: Skipping: {}'.format(mip.label, command)
+        print ' EC-Earth3 does not participate in {:11}: Skipping: {}'.format(mip.label, command)
 
 
 print ' There are {} experiments included. '.format(experiment_counter)
