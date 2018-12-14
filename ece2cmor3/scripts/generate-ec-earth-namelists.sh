4e14cb0b36af2b4ff960bdf406c7727ab6fd4a57
treerink <thomas.reerink.knmi@gmail.com>
2018-12-14 16:47:02 +0100

    Because issue #297 is solved, no pptdddddd-100 files are produced anylonger. However we are not interested so far in hourly ppt files, so remove them now.

--- ece2cmor3/scripts/generate-ec-earth-namelists.sh
+++ ece2cmor3/scripts/generate-ec-earth-namelists.sh
@@ -92,7 +92,7 @@ if [ "$#" -eq 4 ] || [ "$#" -eq 5 ]; then
  #./drq2ppt.py --vars cmip6-data-request/cmip6-data-request-m=${mip_label}-e=${experiment}-t=${tier}-p=${priority}/cmvmm_${mip_label}_${experiment}_${tier}_${priority}.xlsx
 
   mkdir -p ${path_of_created_output_control_files}/file_def-compact
-  if [ -f pptdddddd-100 ]; then rm -f pptdddddd-100 ; fi
+  if [ -f pptdddddd0100 ]; then rm -f pptdddddd0100 ; fi                 # Removing thehourly / sub hourly table variables.
   mv -f ppt0000000000 pptdddddd* ${path_of_created_output_control_files}
 
   # Creating the file_def files for XIOS NEMO input:
