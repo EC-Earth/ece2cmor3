9036a1c19056d291f9effb469d03146411903abf
treerink <thomas.reerink.knmi@gmail.com>
2018-12-20 15:32:51 +0100

    Add rule at highest level to disable the "transect" variables until they will work properly in ec-earth3 #323.

--- ece2cmor3/scripts/genecec.py
+++ ece2cmor3/scripts/genecec.py
@@ -85,6 +85,7 @@ for mip in dq.coll['mip'].items:
        command_6 = "sed -i -e 's/uoce_e3u_vsum_e2u_cumul. freq_op=.1ts/uoce_e3u_vsum_e2u_cumul/' cmip6-output-control-files/" + mip_label + '/cmip6-experiment-' + mip_label + '-' + ex.label + '/file_def_nemo-opa.xml'
        command_7 = 'mkdir -p cmip6-output-control-files/' + mip_name + '/' + model_configuration + '/cmip6-experiment-' + mip_name + '-' + ex.label + '; mv cmip6-output-control-files/' + mip_label + '/cmip6-experiment-' + mip_label + '-' + ex.label + '/*' + ' cmip6-output-control-files/' + mip_name + '/' + model_configuration + '/cmip6-experiment-' + mip_name + '-' + ex.label + '; rm -rf ' + ' cmip6-output-control-files/' + mip_label
        command_8 = '      mv cmip6-output-control-files/' + mip_name + '/' + model_configuration + '/cmip6-experiment-' + mip_name + '-' + ex.label + '/volume-estimate-* cmip6-output-control-files/' + mip_name + '/' + model_configuration + '/cmip6-experiment-' + mip_name + '-' + ex.label + '/volume-estimate-'  + mip_name + '-' + ex.label + '-' + model_configuration + '.txt'
+       command_c = "sed -i 's/enabled=\"True\" field_ref=\"transport/enabled=\"False\" field_ref=\"transport/' cmip6-output-control-files/" + mip_name + '/' + model_configuration + '/cmip6-experiment-' + mip_name + '-' + ex.label + '/file_def_nemo*'
       #print print '{}'.format(command)
        if mip_name in ec_earth_mips:
          #if ex.tier[0] in experiment_tiers_included and ex.label == 'piControl':  # for a fast test
@@ -101,6 +102,7 @@ for mip in dq.coll['mip'].items:
              if mip_name == 'CMIP' and ',' in mip_list:
               os.system(command_7) # Rename directry names for joint MIPs
               os.system(command_8) # Rename volume-estimate file for joint MIPs
+             os.system(command_c)  # Switching the 'transect' variables off (the transect grid definition seems to depend on the XIOS 2.5 upgrade)
              experiment_counter = experiment_counter + 1
           else:
              print ' Tier {} experiments are not included: Skipping: {}'.format(ex.tier[0], command)
@@ -126,9 +128,11 @@ for mip in dq.coll['mip'].items:
        command_4 = "sed -i -e 's/True\" field_ref=\"toce_pot\"/False\" field_ref=\"toce_pot\"/' cmip6-output-control-files/" + mip_label + '/cmip6-experiment-' + mip_label + '-' + ex.label + '/file_def_nemo-opa.xml'
        command_5 = "sed -i -e '/sfdsi_2/d' cmip6-output-control-files/" + mip_label + '/cmip6-experiment-' + mip_label + '-' + ex.label + '/file_def_nemo-opa.xml'
        command_6 = "sed -i -e 's/uoce_e3u_vsum_e2u_cumul. freq_op=.1ts/uoce_e3u_vsum_e2u_cumul/' cmip6-output-control-files/" + mip_label + '/cmip6-experiment-' + mip_label + '-' + ex.label + '/file_def_nemo-opa.xml'
+       command_c = "sed -i 's/enabled=\"True\" field_ref=\"transport/enabled=\"False\" field_ref=\"transport/' cmip6-output-control-files/" + mip_name + '/cmip6-experiment-' + mip_name + '-' + ex.label + '/file_def_nemo*'
       #print print '{}'.format(command)
        if mip_name in ec_earth_mips:
-         #if ex.tier[0] in experiment_tiers_included and ex.label == 'piControl':  # for a faster test
+         #if ex.tier[0] in experiment_tiers_included and ex.label == 'piControl':   # for a faster test
+         #if ex.tier[0] in experiment_tiers_included and ex.label == 'historical':  # for a faster test
           if ex.tier[0] in experiment_tiers_included:
             if ex.label == 'esm-hist' or ex.label == 'esm-piControl':
              print 'Skipping this esm experiment ' + ex.label + ' because its CMIP6 data request fails so far.\n'
@@ -139,6 +143,7 @@ for mip in dq.coll['mip'].items:
             #os.system(command_4)  # Just set the toce fields false again because we still face troubles with them
             #os.system(command_5)  # Delete the line with sfdsi_2 from the file_def_nemo-opa.xml files
              os.system(command_6)  # Remove the freq_op attribute for the variable msftbarot (uoce_e3u_vsum_e2u_cumul) from the file_def_nemo.xml file
+             os.system(command_c)  # Switching the 'transect' variables off (the transect grid definition seems to depend on the XIOS 2.5 upgrade)
              experiment_counter = experiment_counter + 1
           else:
              print ' Tier {} experiments are not included: Skipping: {}'.format(ex.tier[0], command)
@@ -158,4 +163,4 @@ command_b  = "sed -i 's/enabled=\"False\"/enabled=\"True\"/' cmip6-output-contro
 command_c  = "sed -i 's/enabled=\"True\" field_ref=\"transport/enabled=\"False\" field_ref=\"transport/' cmip6-output-control-files/test-all-mips/file_def_nemo-*"
 os.system(command_a) # Create a new subdirectory for testing all available variables in the file_def files
 os.system(command_b) # Switch on all available variables in the file_def files
-os.system(command_c) # Switch of the 'transect' variables (the transect grid definition seems to depend on the XIOS 2.5 upgrade)
+os.system(command_c) # Switching the 'transect' variables off (the transect grid definition seems to depend on the XIOS 2.5 upgrade)
