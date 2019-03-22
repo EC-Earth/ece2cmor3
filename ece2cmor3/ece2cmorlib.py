f66edf3b2439f2a4dd44621b94e4f61434a6f90a
goord <G.vandenOord@gmail.com>
2019-03-22 13:55:40 +0000

    Moved check to a better place

--- ece2cmor3/ece2cmorlib.py
+++ ece2cmor3/ece2cmorlib.py
@@ -169,9 +169,9 @@ def perform_ifs_tasks(datadir, expname,
     validate_setup_settings()
     validate_run_settings(datadir, expname)
     ifs_tasks = [t for t in tasks if t.source.model_component() == "ifs"]
+    log.info("Selected %d IFS tasks from %d input tasks" % (len(ifs_tasks), len(tasks)))
     if len(ifs_tasks) == 0:
         return
-    log.info("Selected %d IFS tasks from %d input tasks" % (len(ifs_tasks), len(tasks)))
     tableroot = os.path.join(table_dir, prefix)
     if enable_masks:
         ifs2cmor.masks = {k: masks[k] for k in masks if masks[k]["source"].model_component() == "ifs"}
