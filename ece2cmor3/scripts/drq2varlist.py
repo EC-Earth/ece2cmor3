ad24712872f107c442a067b9c9236b62ad4bd4f5
treerink <thomas.reerink.knmi@gmail.com>
2019-12-09 09:43:25 +0100

    Add messages for variables which are in the omit list of drq2varlist #542.

--- ece2cmor3/scripts/drq2varlist.py
+++ ece2cmor3/scripts/drq2varlist.py
@@ -95,32 +95,40 @@ def main():
 
             # Taking off several variables from the json data request files:
             skip_case = False
-            # See issue #521:
             if target.variable in ['intdoc']:
+             # See issue #521:
+             log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/521" % (target.table, target.variable))
              skip_case = True
-            # See issue #498 & #469:
             if target.variable in ['rlntds', 'hfibthermds', 'hflso', 'agessc', 'ficeberg', 'hfsso', 'hfcorr', 'wfcorr', 'nwdFracLut']:
+             # See issue #498 & #469:
+             log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/498 & https://github.com/EC-Earth/ece2cmor3/issues/469" % (target.table, target.variable))
              skip_case = True
-            # See issue #609-36 & #609-37 at ece-portal:
             if target.variable in ['hfibthermds2d', 'ficeberg2d', 'fgcfc12']:
+             # See issue #516 and #609-36 & #609-37 at ece-portal:
+             log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/516 & https://dev.ec-earth.org/issues/609#note-36" % (target.table, target.variable))
              skip_case = True
-            # See issue #504:
             if target.variable in ['cfc11', 'fgsf6']:
+             # See issue #504:
+             log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/504" % (target.table, target.variable))
              skip_case = True
-            # See issue #493 & #542:
             if table in ['Oyr'] and target.variable in ['cfc11', 'ocontempdiff', 'ocontemppadvect', 'ocontemppmdiff', 'ocontemprmadvect', 'ocontemptend', 'osaltdiff', 'osaltpadvect', 'osaltpmdiff', 'osaltrmadvect', 'osalttend']:
+             # See issue #493 & #542:
+             log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/493 & https://github.com/EC-Earth/ece2cmor3/issues/542" % (target.table, target.variable))
              skip_case = True
-            # See issue #542:
             if getattr(args, "allvars", True):
-             # Conflicting combinations (skip the 2nd one, an arbitrary choice):
-             # 6hrPlevPt:  ta7h,  ta27
-             # 6hrPlevPt: hus7h, hus27
-             # Emon:        hus, hus27
-             # Emon:         va,  va27
-             # Emon:         ua,  ua27
              if table in ['6hrPlevPt'] and target.variable in ['ta27', 'hus27']:
+              # See issue #542:
+              # Conflicting combinations (skip the 2nd one, an arbitrary choice):
+              # 6hrPlevPt:  ta7h,  ta27
+              # 6hrPlevPt: hus7h, hus27
+              log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/542" % (target.table, target.variable))
               skip_case = True
              if table in ['Emon'] and target.variable in ['hus27', 'va27', 'ua27']:
+              # See issue #542:
+              # Emon:        hus, hus27
+              # Emon:         va,  va27
+              # Emon:         ua,  ua27
+              log.info(" Variable %s %s is listed in the omit list of drq2varlist and therefore skipped. See https://github.com/EC-Earth/ece2cmor3/issues/542" % (target.table, target.variable))
               skip_case = True
 
             if skip_case is False:
