cba18f48ba21667ff6e9aa4ad3a11f7f0c0279e4
tommibergman <tommi.bergman@iki.fi>
2020-01-13 13:45:26 +0200

    Remove erroneous return statement

--- ece2cmor3/tm52cmor.py
+++ ece2cmor3/tm52cmor.py
@@ -520,6 +520,7 @@ def create_cmor_variable(task,dataset,axes):
     if(hasattr(task.target,"positive") and len(task.target.positive) != 0):
         return cmor.variable(table_entry = str(task.target.variable),units = str(unit),axis_ids = axes,original_name = str(srcvar),positive = "down")
     else:
+        print  str(task.target.variable),str(unit),axes,str(srcvar)
         return cmor.variable(table_entry = str(task.target.variable),units = str(unit),axis_ids = axes,original_name = str(srcvar))
 
 def interpolate_plev(pressure_levels,dataset,psdata,varname):
@@ -831,7 +832,7 @@ def create_hybrid_level_axis(task,leveltype='alevel'):
     finally:
         if ds is not None:
             ds.close()
-        return None,None
+        #return None,None
 
 def create_lat():
     """Create latitude dimension
