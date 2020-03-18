edcfe4c4407b4a49a15b49e7976c5cb2b2627481
treerink <thomas.reerink.knmi@gmail.com>
2020-03-18 12:24:18 +0100

    Identification of co2 & co2mass: production by TM5, taking them off from the ignored list #589.

--- ece2cmor3/scripts/update-basic-identifiedmissing-ignored-files.sh
+++ ece2cmor3/scripts/update-basic-identifiedmissing-ignored-files.sh
@@ -86,6 +86,8 @@ if [ "$#" -eq -2 ]; then
 #  -Add manually the Eday   prrc                                                            to the list-of-ignored-cmpi6-requested-variables.xlsx
 #  -Add manually the Omon   prra                                                            to the list-of-ignored-cmpi6-requested-variables.xlsx
 #  -Add manually the E3hrPt rsutcsaf  #556                                                  to the list-of-ignored-cmpi6-requested-variables.xlsx
+#  -Add manually the AERmon  co2  #589                                                      to the list-of-ignored-cmpi6-requested-variables.xlsx
+#  -Add manually the E3hrPt  co2  #589                                                      to the list-of-ignored-cmpi6-requested-variables.xlsx
 
 # Test that this replace gives still the same results:
    mkdir -p ${HOME}/cmorize/ece2cmor3/ece2cmor3/scripts/backup-cmvmm-all-mips-t=3-p=3; rm -f ${HOME}/cmorize/ece2cmor3/ece2cmor3/scripts/backup-cmvmm-all-mips-t=3-p=3/cmvmm-all-mips-t=3-p=3.*;
