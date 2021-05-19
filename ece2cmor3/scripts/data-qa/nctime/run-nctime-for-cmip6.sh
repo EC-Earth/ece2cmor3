d53a1a99438a4b9b08277abd5171c60161a39d5a
Thomas Reerink <thomas.reerink.knmi@gmail.com>
2021-05-19 14:19:18 +0200

    Cleaning, comment correcting #699.

--- ece2cmor3/scripts/data-qa/nctime/run-nctime-for-cmip6.sh
+++ ece2cmor3/scripts/data-qa/nctime/run-nctime-for-cmip6.sh
@@ -1,24 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Thomas Reerink
 #
-# This scripts needs one argument
+# This scripts needs one argument: the directory path with the cmorised data
 #
-# ${1} the first argument is the directory with the cmorised data
-#
-#
-# Run example:
-#  ./run-nctime-for-cmip6.sh CMIP6
+# Run this script without arguments for examples how to call this script.
 #
 
 if [ "$#" -eq 1 ]; then
 
- # Test whether the ece2cmor_root_directory exists:
+ # Test whether the directory with the cmorised data exists:
  if [ ! -d $1 ]; then 
   echo
-  echo ' The directory which was specified for the CMIP6 data: ' $1 ' is not found.'
-  echo ' Stop'
-  exit
+  echo ' Error: The directory which was specified for the CMIP6 data: ' $1 ' is not found.'
   echo
+  exit
  fi
 
  # Install nctcck which is part of nctime with pip:
@@ -28,9 +23,9 @@ if [ "$#" -eq 1 ]; then
  nctcck -i esgini-dir -l log-nctcck $1
 
 else
-    echo '  '
-    echo '  This scripts requires one argument: The directory with the cmorised data, e.g.:'
-    echo '  ' $0 CMIP6
-    echo '  ' $0 /lustre2/projects/model_testing/reerink/cmorised-results/cmor-cmip-historical-for-nsc/cmor-cmip-historical/h003/CMIP6
-    echo '  '
+ echo
+ echo ' This scripts requires one argument: The directory with the cmorised data, e.g.:'
+ echo ' ' $0 CMIP6
+ echo ' ' $0 /lustre2/projects/model_testing/reerink/cmorised-results/cmor-cmip-historical-for-nsc/cmor-cmip-historical/h003/CMIP6
+ echo
 fi
