e1f1de6336ca76f7ea8eacb56c83ed7f6d81471a
treerink <thomas.reerink.knmi@gmail.com>
2023-03-10 12:03:10 +0100

    Make verbose, correct header & clean #749.

--- ece2cmor3/scripts/add-sofiamip-experiments.sh
+++ ece2cmor3/scripts/add-sofiamip-experiments.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 # Thomas Reerink
 #
-# This script adds / inserts some non-cmor variables and /or non (yet) approved CMIP endorsed experiments (which thus
-# do not exit in the CMIP6 data request) to the end of the cmor table in question.
+# This script adds / inserts a not (yet) approved CMIP endorsed MIP and experiment(s),
+# which thus do not exist in the CMIP6 CMOR tables.
 #
 # This scripts requires no arguments.
-#
-# Run example:
-#  ./add-sofiamip-experiments.sh
 #
 
 if [ "$#" -eq 0 ]; then
@@ -22,32 +19,6 @@ if [ "$#" -eq 0 ]; then
   cd ../resources/cmip6-cmor-tables
   git checkout Tables/CMIP6_CV.json
   cd -
-
-  #           "faf-antwater-stress":{
-  #               "activity_id":[
-  #                   "FAFMIP"
-  #               ],
-  #               "additional_allowed_model_components":[
-  #                   "AER",
-  #                   "CHEM",
-  #                   "BGC"
-  #               ],
-  #               "experiment":"control plus perturbative surface fluxes of momentum and freshwater into ocean, the latter around the coast of Antarctica only",
-  #               "experiment_id":"faf-antwater-stress",
-  #               "parent_activity_id":[
-  #                   "CMIP"
-  #               ],
-  #               "parent_experiment_id":[
-  #                   "piControl"
-  #               ],
-  #               "required_model_components":[
-  #                   "AOGCM"
-  #               ],
-  #               "sub_experiment_id":[
-  #                   "none"
-  #               ]
-  #           },
-
 
 # sed -i  '/"faf-antwater-stress":{/i \
 #           "faf-antwater":{                                                                                                                                  \
@@ -86,15 +57,22 @@ if [ "$#" -eq 0 ]; then
   # Remove the trailing spaces of the inserted block above:
   sed -i 's/\s*$//g' ../resources/tables/CMIP6_CV.json
 
+  echo
+  echo " $0 reports:"
+  echo "  The adjusted file is:  ../resources/tables/CMIP6_CV.json "
+  echo "  Which is part of a nested repository, therefore to view the diff, run:"
+  echo "  cd ../resources/tables; git diff; cd -"
+  echo
+
  else
-    echo '  '
-    echo '  Nothing done, no set of variables and / or experiments has been selected to add to the tables.'
-    echo '  '
+    echo
+    echo " Nothing done, no set of variables and / or experiments has been selected to add to the tables."
+    echo
  fi
 
 else
-    echo '  '
-    echo '  This scripts requires no argument:'
-    echo '  ' $0
-    echo '  '
+    echo
+    echo " This scripts requires no argument:"
+    echo "  $0"
+    echo
 fi
