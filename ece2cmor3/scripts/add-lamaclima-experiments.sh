49c7629f93cde170d5a80fcfdb9598cd5ffa33f7
treerink <thomas.reerink.knmi@gmail.com>
2023-03-10 12:47:19 +0100

    Generalize script, complement description #645.

--- ece2cmor3/scripts/add-lamaclima-experiments.sh
+++ ece2cmor3/scripts/add-lamaclima-experiments.sh
@@ -12,16 +12,21 @@
 
 if [ "$#" -eq 0 ]; then
 
- add_the_lamaclima_experiments=True             # See #853
- 
+ add_the_lamaclima_experiments=True
 
  if [ add_the_lamaclima_experiments ]; then
-  # See #853
-  #  LAMACLIMA ssp585-lamaclima
+  # See #645  https://github.com/EC-Earth/ece2cmor3/issues/645
+  # See #699  https://github.com/EC-Earth/ece2cmor3/issues/699
+  # See #739  https://github.com/EC-Earth/ece2cmor3/issues/739
+  # See #853  https://dev.ec-earth.org/issues/853
 
-  cd ../resources/cmip6-cmor-tables
-  git checkout Tables/CMIP6_CV.json
-  cd -
+  # LAMACLIMA ssp585-lamaclima
+
+  table_path=../resources/cmip6-cmor-tables/Tables/
+  table_file_cv=CMIP6_CV.json
+
+  cd ${table_path}
+  git checkout ${table_file_cv}
 
   sed -i  '/"ssp585-withism":{/i \
             "ssp585-lamaclima":{                                       \
@@ -46,14 +51,23 @@ if [ "$#" -eq 0 ]; then
                     "none"                                             \
                 ]                                                      \
             },                                                         
-  ' ../resources/tables/CMIP6_CV.json
+  ' ${table_file_cv}
 
   sed -i  '/"LS3MIP":/i \
             "LAMACLIMA":"LAnd MAnagement for CLImate Mitigation and Adaptation",
-  ' ../resources/tables/CMIP6_CV.json
+  ' ${table_file_cv}
 
   # Remove the trailing spaces of the inserted block above:
-  sed -i 's/\s*$//g' ../resources/tables/CMIP6_CV.json
+  sed -i 's/\s*$//g' ${table_file_cv}
+
+  cd -
+
+  echo
+  echo " $0 reports:"
+  echo "  The adjusted file is:  ${table_path}/${table_file_cv}"
+  echo "  Which is part of a nested repository, therefore to view the diff, run:"
+  echo "  cd ${table_path}; git diff; cd -"
+  echo
 
  else
     echo '  '
