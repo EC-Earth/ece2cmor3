95b9c1f3bbaa298a8cf8c7038117d039bb150261
treerink <thomas.reerink.knmi@gmail.com>
2023-03-10 13:35:29 +0100

    More verbose and generalize / unify #728.

--- ece2cmor3/scripts/add-varex-experiments.sh
+++ ece2cmor3/scripts/add-varex-experiments.sh
@@ -17,27 +17,37 @@ if [ "$#" -eq 0 ]; then
 
   # VAREX
 
-  # Always first revert the CMOR table file in its repository:
-  cd ../resources/cmip6-cmor-tables
-  git checkout Tables/CMIP6_CV.json
-  cd -
+  table_path=../resources/cmip6-cmor-tables/Tables/
+  table_file_cv=CMIP6_CV.json
 
+  # Always first revert the CMOR table file in its repository:
+  cd ${table_path}
+  git checkout ${table_file_cv}
 
   # Add KNMI as an institute with its own institution_id:
   sed -i  '/"KIOST":/i \
             "KNMI":"The Royal Netherlands Meteorological Institute (KNMI), De Bilt, The Netherlands",
-  ' ../resources/tables/CMIP6_CV.json
+  ' ${table_file_cv}
 
   # Adjust the license such that it matches with the production institute KNMI.
-  sed -i -e 's/CMIP6 model data/The VAREX model data/' -e 's/Consult.*acknowledgment//' ../resources/tables/CMIP6_CV.json
+  sed -i -e 's/CMIP6 model data/The VAREX model data/' -e 's/Consult.*acknowledgment//' ${table_file_cv}
 
   # Allow KNMI on it self to be a institute which produces EC-Earth3 experiments:
   # This insert is vulnerable for upstream table changes within 20 lines after the match:
-  sed -i -e '/"EC-Earth3":{/!b;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;a\                    "KNMI",' ../resources/tables/CMIP6_CV.json
+  sed -i -e '/"EC-Earth3":{/!b;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;a\                    "KNMI",' ${table_file_cv}
 
   # Allow a historical experiment to have a parent_experiment_id which is an historical experiment as well:
   # This insert is vulnerable for upstream table changes within 15 lines after the match:
-  sed -i -e '/            "historical":{/!b;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;a\                    "historical",' ../resources/tables/CMIP6_CV.json
+  sed -i -e '/            "historical":{/!b;n;n;n;n;n;n;n;n;n;n;n;n;n;n;n;a\                    "historical",' ${table_file_cv}
+
+  cd -
+
+  echo
+  echo " $0 reports:"
+  echo "  The adjusted file is:  ${table_path}/${table_file_cv}"
+  echo "  Which is part of a nested repository, therefore to view the diff, run:"
+  echo "  cd ${table_path}; git diff; cd -"
+  echo
 
  else
     echo '  '
