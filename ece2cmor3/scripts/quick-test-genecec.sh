a263becdd907bdcc5bcf245eadae3debc00a746c
treerink <thomas.reerink.knmi@gmail.com>
2023-03-18 00:18:10 +0100

    Remove/clean environment activate instructions.

--- ece2cmor3/scripts/quick-test-genecec.sh
+++ ece2cmor3/scripts/quick-test-genecec.sh
@@ -40,20 +40,12 @@ if [ "$#" -eq 2 ]; then
   mip=CMIP
   exp=historical
 
- #if [ "${python_version}" == 'python-2' ]; then
- # activateece2cmor3bymambapy2
- #else
- # activateece2cmor3bymambapy3
- #fi
-
   cd ${HOME}/cmorize/ece2cmor3-${python_version}/ece2cmor3/scripts
   test_dir=../../../control-output-files/quicktest/quicktest-${python_version}
   log_dir=${test_dir}/log-files/; mkdir -p ${log_dir}
   log_file=${log_dir}/log-${mip}-${exp}-${test_version}.txt
   ./genecec-per-mip-experiment.sh ${test_dir}/${mip}-${exp}-${test_version} ${mip_list} ${exp} 1 1 >& ${log_file}
   sed -i -e 's/^.*INFO:ece2cmor3/INFO:ece2cmor3/' -e 's/^.*WARNING:ece2cmor3/WARNING:ece2cmor3/' -e 's/^.*INFO:root/INFO:root/' ${log_file}
-  
- #conda deactivate
 
   echo " Compare the results like:"
   echo "  diff -r ${test_dir}/${mip}-${exp}-v01 ${test_dir}/${mip}-${exp}-${test_version}"
@@ -61,14 +53,8 @@ if [ "$#" -eq 2 ]; then
 
 else
   echo
-  echo "  This scripts has to be run with the correct environment and requires two arguments, e.g.:"
-  echo "   activateece2cmor3bymambapy2"
-  echo "   $0 python-2 v01"
-  echo
-  echo "   activateece2cmor3bymambapy3"
-  echo "   $0 python-3 v01"
+  echo " This scripts has to be run with the correct environment and requires two arguments, e.g.:"
+  echo "  $0 python-2 v01"
+  echo "  $0 python-3 v01"
   echo
 fi
-
-# A quick one liner to test the taskloader:
-#  drq2ppt --drq cmip6-data-request/cmip6-data-request-CMIP.DCPP.LS3MIP.PAMIP.RFMIP.ScenarioMIP.VolMIP.CORDEX.DynVarMIP.SIMIP.VIACSAB-historical-t1-p1/cmvme_cm.co.dc.dy.ls.pa.rf.sc.si.vi.vo_historical_1_1.xlsx; mkdir -p test-drq-ppt-v01; mv -f ppt* volume-estimate-ifs.txt test-drq-ppt-v01; diff -r test-drq-ppt-v01/ test-drq-ppt-v00/
