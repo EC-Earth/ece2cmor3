c8b7dc10f644ce005f86f51ebfc8a3eed3fc3249
P. Le Sager <sager@knmi.nl>
2023-03-29 13:42:06 +0200

    Add ice crystal diagnostics and make them available in AERmon

--- ece2cmor3/scripts/add-non-cmor-variables.sh
+++ ece2cmor3/scripts/add-non-cmor-variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Thomas Reerink
 #
-# This script adds some non-cmor variables (which thus do not exit in the CMIP6 data request) in the relevant cmor table.
+# This script adds some non-cmor variables (which thus do not exit in
+# the CMIP6 data request) to the AERmon cmor table.
 #
-# This scripts requires no arguments.
+# This script requires no arguments.
 #
 # Run example:
 #  ./add-non-cmor-variables.sh
@@ -23,6 +24,8 @@ if [ "$#" -eq 0 ]; then
   # rssaf    126071  CVEXTR2(12)='total SW surf', grib 126.71 total SW surf rssaf
   # rlscsaf  126074  CVEXTR2(15)='clear LW surf', grib 126.74 clear LW surf rlscsaf
   # rlsaf    126075  CVEXTR2(16)='total LW surf', grib 126.75 total LW surf rlsaf
+
+  # Also add the AerChem related Ice Crystal variables (see https://dev.ec-earth.org/issues/1079)
 
   table_path=../resources/cmip6-cmor-tables/Tables/
   table_file_aermon=CMIP6_AERmon.json
@@ -102,7 +105,115 @@ if [ "$#" -eq 0 ]; then
             "valid_max": "",                                                                                                    \
             "ok_min_mean_abs": "",                                                                                              \
             "ok_max_mean_abs": ""                                                                                               \
-        },                                                                                                                      
+        },                                                                                                                      \
+        "icnc": { \
+            "frequency": "mon",  \
+            "modeling_realm": "aerosol",  \
+            "standard_name": "number_concentration_of_cloud_ice_crystal_particles_in_air",  \
+            "units": "m-3",  \
+            "cell_methods": "area: time: mean",  \
+            "cell_measures": "area: areacella",  \
+            "long_name": "Ice Crystal Number Concentration",  \
+            "comment": "Ice Crystal Number Concentration in ice clouds.",  \
+            "dimensions": "longitude latitude alevel time",  \
+            "out_name": "icnc",  \
+            "type": "real",  \
+            "positive": "",  \
+            "valid_min": "",  \
+            "valid_max": "",  \
+            "ok_min_mean_abs": "",  \
+            "ok_max_mean_abs": "" \
+        },  \
+        "icncsip": { \
+            "frequency": "mon", \
+            "modeling_realm": "aerosol", \
+            "standard_name": "number_concentration_of_secondary_cloud_ice_crystal_particles_in_air", \
+            "units": "m-3", \
+            "cell_methods": "area: time: mean", \
+            "cell_measures": "area: areacella", \
+            "long_name": "Ice Crystal Number Concentration from secondary formation", \
+            "comment": "Ice Crystal Number Concentration formed by secondary ice production processes.", \
+            "dimensions": "longitude latitude alevel time", \
+            "out_name": "icncsip", \
+            "type": "real", \
+            "positive": "", \
+            "valid_min": "", \
+            "valid_max": "", \
+            "ok_min_mean_abs": "", \
+            "ok_max_mean_abs": "" \
+        }, \
+        "icncullrichdn": { \
+            "frequency": "mon", \
+            "modeling_realm": "aerosol", \
+            "standard_name": "number_concentration_of_cloud_ice_crystal_particles_in_air_from_ullrich_dn", \
+            "units": "m-3", \
+            "cell_methods": "area: time: mean", \
+            "cell_measures": "area: areacella", \
+            "long_name": "Ice Crystal Number Concentration from Ullrich et al. (2017) deposition nucleation of soot and dust parameterization", \
+            "comment": "Ice Crystal Number Concentration formed from Ullrich et al. (2017) deposition nucleation of soot and dust parameterization.", \
+            "dimensions": "longitude latitude alevel time", \
+            "out_name": "icncullrichdn", \
+            "type": "real", \
+            "positive": "", \
+            "valid_min": "", \
+            "valid_max": "", \
+            "ok_min_mean_abs": "", \
+            "ok_max_mean_abs": "" \
+        }, \
+        "icncharrison": { \
+            "frequency": "mon", \
+            "modeling_realm": "aerosol", \
+            "standard_name": "number_concentration_of_cloud_ice_crystal_particles_in_air_from_harrison", \
+            "units": "m-3", \
+            "cell_methods": "area: time: mean", \
+            "cell_measures": "area: areacella", \
+            "long_name": "Ice Crystal Number Concentration from Harrison et al. (2019) immersion freezing of K-feldspar and quartz parameterization", \
+            "comment": "Ice Crystal Number Concentration formed from Harrison et al. (2019) immersion freezing of K-feldspar and quartz parameterization.", \
+            "dimensions": "longitude latitude alevel time", \
+            "out_name": "icncharrison", \
+            "type": "real", \
+            "positive": "", \
+            "valid_min": "", \
+            "valid_max": "", \
+            "ok_min_mean_abs": "", \
+            "ok_max_mean_abs": "" \
+        }, \
+        "icncwilson": { \
+            "frequency": "mon", \
+            "modeling_realm": "aerosol", \
+            "standard_name": "number_concentration_of_cloud_ice_crystal_particles_in_air_from_wilson", \
+            "units": "m-3", \
+            "cell_methods": "area: time: mean", \
+            "cell_measures": "area: areacella", \
+            "long_name": "Ice Crystal Number Concentration from Wilson et al. (2015) immersion freezing of marine organic aerosols parameterization", \
+            "comment": "Ice Crystal Number Concentration formed from Wilson et al. (2015) immersion freezing of marine organic aerosols parameterization.", \
+            "dimensions": "longitude latitude alevel time", \
+            "out_name": "icncwilson", \
+            "type": "real", \
+            "positive": "", \
+            "valid_min": "", \
+            "valid_max": "", \
+            "ok_min_mean_abs": "", \
+            "ok_max_mean_abs": "" \
+        }, \
+        "icncpip": { \
+            "frequency": "mon", \
+            "modeling_realm": "aerosol", \
+            "standard_name": "number_concentration_of_primary_cloud_ice_crystal_particles_in_air", \
+            "units": "m-3", \
+            "cell_methods": "area: time: mean", \
+            "cell_measures": "area: areacella", \
+            "long_name": "Ice Crystal Number Concentration from primary formation", \
+            "comment": "Ice Crystal Number Concentration formed as primary ice.", \
+            "dimensions": "longitude latitude alevel time", \
+            "out_name": "icncpip", \
+            "type": "real", \
+            "positive": "", \
+            "valid_min": "", \
+            "valid_max": "", \
+            "ok_min_mean_abs": "", \
+            "ok_max_mean_abs": ""  \
+        },
   ' ${table_file_aermon}
 
   # Remove the trailing spaces of the inserted block above:
