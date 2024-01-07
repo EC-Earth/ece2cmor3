4a9150ed5560b65602d7c32e5a0ddb5bb1b8a745
Thomas Reerink <thomas.reerink.knmi@gmail.com>
2024-01-07 19:47:26 +0000

    Move foci minimum test request #775.

--- ece2cmor3/scripts/submit-script-examples/submit-at-hpc2020-ece2cmor-leg-job-foci.sh
+++ ece2cmor3/scripts/submit-script-examples/submit-at-hpc2020-ece2cmor-leg-job-foci.sh
@@ -36,8 +36,7 @@
    ECEMODEL=EC-EARTH-AerChem
    METADATA=${PERM}/ec-earth-3/r9728-foci-output/runtime/classic/ctrl/output-control-files/foci/metadata-cmip6-CMIP-historical-EC-EARTH-AerChem-$COMPONENT-template.json
    TEMPDIR=${SCRATCH}/temp-cmor-dir/$EXP/$COMPONENT/$LEG
-  #VARLIST=${PWD}/../../resources/test-data-request/varlist-foci.json
-  #VARLIST=${PWD}/../../resources/miscellaneous-data-requests/foci-request/varlist-foci.json
+  #VARLIST=${PWD}/../../resources/miscellaneous-data-requests/foci-request/varlist-foci-minimal-test.json
    VARLIST=${PWD}/../../resources/miscellaneous-data-requests/foci-request/full-foci-varlist.json
    ODIR=${SCRATCH}/cmorised-results/foci/$EXP/$VERSION
 
