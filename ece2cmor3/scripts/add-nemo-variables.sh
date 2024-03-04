5d4cfed5ea822cbd31faee792eeddaa80e98d1b2
treerink <thomas.reerink.knmi@gmail.com>
2024-03-04 14:07:02 +0100

    Add the non-cmor ubar, vbar, mlddzt & hcont300 to the Omon table #814.

--- ece2cmor3/scripts/add-nemo-variables.sh
+++ ece2cmor3/scripts/add-nemo-variables.sh
@@ -14,7 +14,8 @@ if [ "$#" -eq 1 ]; then
  do_clean=$1
 
  if [ ${do_clean} == 'clean-before' ] || [ ${do_clean} == 'no-clean-before' ]; then
-  # See #802       https://github.com/EC-Earth/ece2cmor3/issues/#802
+  # See #811       https://github.com/EC-Earth/ece2cmor3/issues/811
+  # See #814       https://github.com/EC-Earth/ece2cmor3/issues/814
   # See #1312-146  https://dev.ec-earth.org/issues/1312#note-146
 
   # Overview of added seaIce variables:
@@ -29,6 +30,7 @@ if [ "$#" -eq 1 ]; then
   table_file_cv=CMIP6_CV.json
   table_file_SIday=CMIP6_SIday.json
   table_file_SImon=CMIP6_SImon.json
+  table_file_Omon=CMIP6_Omon.json
   tmp_file_SIday=CMIP6_SIday_tmp.json
 
   cd ${table_path}
@@ -36,6 +38,7 @@ if [ "$#" -eq 1 ]; then
    git checkout ${table_file_cv}
    git checkout ${table_file_SIday}
    git checkout ${table_file_SImon}
+   git checkout ${table_file_Omon}
   fi
 
   sed -i  '/"dec":"decadal mean samples"/i \
@@ -63,31 +66,118 @@ if [ "$#" -eq 1 ]; then
 
   # SImon sfdsi has been used as template:
   sed -i  '/"sidmassdyn": {/i \
-        "siflsaltbot": {                                                                \
-            "frequency": "mon",                                                         \
-            "modeling_realm": "seaIce",                                                 \
-            "standard_name": "total_flux_of_salt_from_water_into_sea_ice",              \
-            "units": "kg m-2 s-1",                                                      \
-            "cell_methods": "area: time: mean where sea_ice (comment: mask=siconc)",    \
-            "cell_measures": "area: areacello",                                         \
-            "long_name": "Total flux from water into sea ice",                          \
+        "siflsaltbot": {                                                                        \
+            "frequency": "mon",                                                                 \
+            "modeling_realm": "seaIce",                                                         \
+            "standard_name": "total_flux_of_salt_from_water_into_sea_ice",                      \
+            "units": "kg m-2 s-1",                                                              \
+            "cell_methods": "area: time: mean where sea_ice (comment: mask=siconc)",            \
+            "cell_measures": "area: areacello",                                                 \
+            "long_name": "Total flux from water into sea ice",                                  \
             "comment": "Total flux of salt from water into sea ice divided by grid-cell area; salt flux is upward (negative) during ice growth when salt is embedded into the ice and downward (positive) during melt when salt from sea ice is again released to the ocean.",                                                               \
-            "dimensions": "longitude latitude time",                                    \
-            "out_name": "sfdsi",                                                        \
-            "type": "real",                                                             \
-            "positive": "down",                                                         \
-            "valid_min": "",                                                            \
-            "valid_max": "",                                                            \
-            "ok_min_mean_abs": "",                                                      \
-            "ok_max_mean_abs": ""                                                       \
-        },                                                                              
+            "dimensions": "longitude latitude time",                                            \
+            "out_name": "sfdsi",                                                                \
+            "type": "real",                                                                     \
+            "positive": "down",                                                                 \
+            "valid_min": "",                                                                    \
+            "valid_max": "",                                                                    \
+            "ok_min_mean_abs": "",                                                              \
+            "ok_max_mean_abs": ""                                                               \
+        }, 
   ' ${table_file_SImon}
+
+
+  # Add four ocean variables to the Omon table:
+  sed -i  '/"umo": {/i \
+        "ubar": {                                                                               \
+            "frequency": "mon",                                                                 \
+            "modeling_realm": "ocean",                                                          \
+            "standard_name": "ocean_barotropic_current_along_i_axis",                           \
+            "units": "m s-1",                                                                   \
+            "cell_methods": "area: mean where sea time: mean",                                  \
+            "cell_measures": "area: areacello",                                                 \
+            "long_name": "Ocean Barotropic Current along i-axis",                               \
+            "comment": "",                                                                      \
+            "dimensions": "longitude latitude time",                                            \
+            "out_name": "ubar",                                                                 \
+            "type": "real",                                                                     \
+            "positive": "",                                                                     \
+            "valid_min": "",                                                                    \
+            "valid_max": "",                                                                    \
+            "ok_min_mean_abs": "",                                                              \
+            "ok_max_mean_abs": ""                                                               \
+        },
+  ' ${table_file_Omon}
+
+  sed -i  '/"vmo": {/i \
+        "vbar": {                                                                               \
+            "frequency": "mon",                                                                 \
+            "modeling_realm": "ocean",                                                          \
+            "standard_name": "ocean_barotropic_current_along_j_axis",                           \
+            "units": "m s-1",                                                                   \
+            "cell_methods": "area: mean where sea time: mean",                                  \
+            "cell_measures": "area: areacello",                                                 \
+            "long_name": "Ocean Barotropic Current along j-axis",                               \
+            "comment": "",                                                                      \
+            "dimensions": "longitude latitude time",                                            \
+            "out_name": "vbar",                                                                 \
+            "type": "real",                                                                     \
+            "positive": "",                                                                     \
+            "valid_min": "",                                                                    \
+            "valid_max": "",                                                                    \
+            "ok_min_mean_abs": "",                                                              \
+            "ok_max_mean_abs": ""                                                               \
+        },
+  ' ${table_file_Omon}
+
+  sed -i  '/"mlotst": {/i \
+        "mlddzt": {                                                                             \
+            "frequency": "mon",                                                                 \
+            "modeling_realm": "ocean",                                                          \
+            "standard_name": "thermocline_depth",                                               \
+            "units": "m",                                                                       \
+            "cell_methods": "area: mean where sea time: mean",                                  \
+            "cell_measures": "area: areacello",                                                 \
+            "long_name": "Thermocline Depth (depth of max dT/dz)",                              \
+            "comment": "depth at maximum upward derivative of sea water potential temperature", \
+            "dimensions": "longitude latitude time",                                            \
+            "out_name": "mlddzt",                                                               \
+            "type": "real",                                                                     \
+            "positive": "",                                                                     \
+            "valid_min": "",                                                                    \
+            "valid_max": "",                                                                    \
+            "ok_min_mean_abs": "",                                                              \
+            "ok_max_mean_abs": ""                                                               \
+        },
+  ' ${table_file_Omon}
+
+  sed -i  '/"hfbasin": {/i \
+        "hcont300": {                                                                           \
+            "frequency": "mon",                                                                 \
+            "modeling_realm": "ocean",                                                          \
+            "standard_name": "heat_content_for_0_300m_top_layer",                               \
+            "units": "J m-2",                                                                   \
+            "cell_methods": "area: mean where sea time: mean",                                  \
+            "cell_measures": "area: areacello",                                                 \
+            "long_name": "Heat content 0-300m",                                                 \
+            "comment": "",                                                                      \
+            "dimensions": "longitude latitude time",                                            \
+            "out_name": "hcont300",                                                             \
+            "type": "real",                                                                     \
+            "positive": "",                                                                     \
+            "valid_min": "",                                                                    \
+            "valid_max": "",                                                                    \
+            "ok_min_mean_abs": "",                                                              \
+            "ok_max_mean_abs": ""                                                               \
+        },
+  ' ${table_file_Omon}
 
 
   # Remove the trailing spaces of the inserted block above:
   sed -i -e 's/\s*$//g'                ${table_file_cv}
   sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_SIday}
   sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_SImon}
+  sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_Omon}
 
   cd -
 
@@ -98,6 +188,7 @@ if [ "$#" -eq 1 ]; then
   echo "  ${table_path}/${table_file_cv}"
   echo "  ${table_path}/${table_file_SIday}"
   echo "  ${table_path}/${table_file_SImon}"
+  echo "  ${table_path}/${table_file_Omon}"
   echo " which is part of the nested CMOR Table repository. View the diff by running:"
   echo "  cd ${table_path}; git diff; cd -"
   echo " This changes can be reverted by running:"
