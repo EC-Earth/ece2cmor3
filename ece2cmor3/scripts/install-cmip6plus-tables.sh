403847db805a792ea061fcb7bbefe669dc285105
treerink <thomas.reerink.knmi@gmail.com>
2025-02-26 00:00:11 +0100

    Clean install-cmip6plus-tables.sh script #841.

--- ece2cmor3/scripts/install-cmip6plus-tables.sh
+++ ece2cmor3/scripts/install-cmip6plus-tables.sh
@@ -14,9 +14,9 @@ if [ "$#" -eq 1 ]; then
 
  echo
  if [ ! -d ${dir_for_tables} ]; then
-   echo "Create ${dir_for_tables}"
-   echo
-   mkdir -p ${dir_for_tables}
+  echo "Create ${dir_for_tables}"
+  echo
+  mkdir -p ${dir_for_tables}
  fi
 
  cd ${dir_for_tables}
@@ -39,29 +39,20 @@ if [ "$#" -eq 1 ]; then
   git clone https://github.com/PCMDI/mip-cmor-tables
  fi
 
- # Create a new directory which from structure resembles the CMIP6 tables repository with links to the two repositories which contain for CMIP6Plus the required files:
+ # Create a new directory which from structure resembles the CMIP6 tables repository
+ # with links to the two repositories which contain for CMIP6Plus the required files:
  mkdir -p cmip6plus-tables
 
  # Populate this new directory with the links to the concerning files:
  cd cmip6plus-tables
  ln -sf ${dir_for_tables}/mip-cmor-tables/{Auxillary_files,Tables}/* .
  ln -sf ${dir_for_tables}/CMIP6Plus_CVs/CVs/CMIP6Plus_CV.json .
- 
- # Then go to your ece2cmor3 directory, activate your conda env as usual, and run
 
  echo
  echo "In order to make use of the CMI6Plus tables set the following optional ece2cmor arguments:"
  echo " --tabledir ${dir_for_tables}/cmip6plus-tables"
  echo " --tableprefix MIP"
  echo
-
- #ece2cmor --varlist varlist.json \
- #         --meta metadata-nemo.json \
- #         --nemo \
- #         --exp $EXPID \
- #         --tabledir ${dir_for_tables}/cmip6plus-tables \
- #         --tableprefix MIP \
- #         /path/to/$EXPID/output/nemo/$LEG
 
 else
   echo
@@ -70,5 +61,3 @@ else
   echo "  $0 \${PERM}/cmorize/"
   echo
 fi
-
-
