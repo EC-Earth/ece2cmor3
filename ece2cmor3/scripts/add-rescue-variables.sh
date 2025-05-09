4ce694906540bcb15955483e98e38810d10fdf73
treerink <thomas.reerink.knmi@gmail.com>
2025-05-09 13:09:42 +0200

    Adress #855 other points.

--- ece2cmor3/scripts/add-rescue-variables.sh
+++ ece2cmor3/scripts/add-rescue-variables.sh
@@ -23,6 +23,8 @@ if [ "$#" -eq 0 ]; then
   #  cBECCS                 Carbon content in CCS from Bioenergy crops (Carbon mass from BE in storage)
   #  fBECCS                 Carbon flux from Bioenergy crops harvested into CCS
   #  fBEatm                 Carbon Flux from Bioenergy crops harvested but not stored (going back into the atmosphere)
+  #  fStemManaf
+  #  cStemManaf
 
   #  From the CO2-Boxmodel:
   #  cDACCS                 Carbon content in CCS from direct Air capture
@@ -36,6 +38,8 @@ if [ "$#" -eq 0 ]; then
   #  cBECCS:    cVeg  (annual)
   #  fBECCS:    fFire (annual)
   #  fBEatm:    fFire (annual)
+  #  fStemManaf fFire (annual)
+  #  cStemManaf cVeg  (annual)
   #  cDACCS:    co2mass
   #  fDACCS:    This one is a global single value no template yet
 
@@ -45,15 +49,53 @@ if [ "$#" -eq 0 ]; then
   ./add-optimesm-variables.sh
 
   table_path=../resources/cmip6-cmor-tables/Tables
-  table_file_LPJGyr=CMIP6_LPJGyr.json
+ #table_file_LPJGyr=CMIP6_LPJGyr.json
   table_file_Eyr=CMIP6_Eyr.json
 
   cd ${table_path}
  #rm -f ${table_file_LPJGyr}      # Do not apply because of the optimes modifications
  #git checkout ${table_file_Eyr}  # Do not apply because of the optimes modifications
 
-  # CHECK UNITS
-  sed -i  '/"pastureFrac": {/i \
+ #sed -i  '/"pastureFrac": {/i \
+ #' ${table_file_LPJGyr}
+
+  sed -i  '/"cLitter": {/i \
+        "cDACCS": {                                                           \
+            "frequency": "yr",                                                \
+            "modeling_realm": "atmos",                                        \
+            "standard_name": "carbon_content_in_ccs_from_direct_air_capture", \
+            "units": "kg",                                                    \
+            "cell_methods": "area: time: mean",                               \
+            "cell_measures": "",                                              \
+            "long_name": "Carbon content in CCS from direct Air capture",     \
+            "comment": "Direct Air-Carbon Capture and Storage (DACCS)",       \
+            "dimensions": "time",                                             \
+            "out_name": "cDACCS",                                             \
+            "type": "real",                                                   \
+            "positive": "",                                                   \
+            "valid_min": "",                                                  \
+            "valid_max": "",                                                  \
+            "ok_min_mean_abs": "",                                            \
+            "ok_max_mean_abs": ""                                             \
+        },                                                                    \
+        "fDACCS": {                                                           \
+            "frequency": "yr",                                                \
+            "modeling_realm": "atmos",                                        \
+            "standard_name": "carbon_flux_into_ccs_from_direct_air_capture",  \
+            "units": "1e-06",                                                 \
+            "cell_methods": "time: mean",                                     \
+            "cell_measures": "area: areacella",                               \
+            "long_name": "Carbon flux into CCS from direct Air capture",      \
+            "comment": "Direct Air-Carbon Capture and Storage (DACCS)",       \
+            "dimensions": "longitude latitude time",                          \
+            "out_name": "fDACCS",                                             \
+            "type": "real",                                                   \
+            "positive": "",                                                   \
+            "valid_min": "",                                                  \
+            "valid_max": "",                                                  \
+            "ok_min_mean_abs": "",                                            \
+            "ok_max_mean_abs": ""                                             \
+        },
         "cBECCS": {                                                                                    \
             "frequency": "yrPt",                                                                       \
             "modeling_realm": "land",                                                                  \
@@ -108,49 +150,46 @@ if [ "$#" -eq 0 ]; then
             "ok_min_mean_abs": "",                                                                     \
             "ok_max_mean_abs": ""                                                                      \
         },
-  ' ${table_file_LPJGyr}
-
-  sed -i  '/"cLitter": {/i \
-        "cDACCS": {                                                           \
-            "frequency": "yr",                                                \
-            "modeling_realm": "atmos",                                        \
-            "standard_name": "carbon_content_in_ccs_from_direct_air_capture", \
-            "units": "kg",                                                    \
-            "cell_methods": "area: time: mean",                               \
-            "cell_measures": "",                                              \
-            "long_name": "Carbon content in CCS from direct Air capture",     \
-            "comment": "Direct Air-Carbon Capture and Storage (DACCS)",       \
-            "dimensions": "time",                                             \
-            "out_name": "cDACCS",                                             \
-            "type": "real",                                                   \
-            "positive": "",                                                   \
-            "valid_min": "",                                                  \
-            "valid_max": "",                                                  \
-            "ok_min_mean_abs": "",                                            \
-            "ok_max_mean_abs": ""                                             \
-        },                                                                    \
-        "fDACCS": {                                                           \
-            "frequency": "yr",                                                \
-            "modeling_realm": "atmos",                                        \
-            "standard_name": "carbon_flux_into_ccs_from_direct_air_capture",  \
-            "units": "1e-06",                                                 \
-            "cell_methods": "time: mean",                                     \
-            "cell_measures": "area: areacella",                               \
-            "long_name": "Carbon flux into CCS from direct Air capture",      \
-            "comment": "Direct Air-Carbon Capture and Storage (DACCS)",       \
-            "dimensions": "longitude latitude time",                          \
-            "out_name": "fDACCS",                                             \
-            "type": "real",                                                   \
-            "positive": "",                                                   \
-            "valid_min": "",                                                  \
-            "valid_max": "",                                                  \
-            "ok_min_mean_abs": "",                                            \
-            "ok_max_mean_abs": ""                                             \
+        "cStemManaf": {                                                                                \
+            "frequency": "yrPt",                                                                       \
+            "modeling_realm": "land",                                                                  \
+            "standard_name": "stem_carbon_in_managed_forest",                                          \
+            "units": "kg m-2",                                                                         \
+            "cell_methods": "area: mean where land time: point",                                       \
+            "cell_measures": "area: areacella",                                                        \
+            "long_name": "Total standing stem carbon in managed forest",                               \
+            "comment": "Total carbon in stems of standing trees in a managed forest",                  \
+            "dimensions": "longitude latitude time1",                                                  \
+            "out_name": "cStemManaf",                                                                  \
+            "type": "real",                                                                            \
+            "positive": "",                                                                            \
+            "valid_min": "",                                                                           \
+            "valid_max": "",                                                                           \
+            "ok_min_mean_abs": "",                                                                     \
+            "ok_max_mean_abs": ""                                                                      \
+        },                                                                                             \
+        "fStemManaf": {                                                                                \
+            "frequency": "yr",                                                                         \
+            "modeling_realm": "land",                                                                  \
+            "standard_name": "harvested_stem_carbon_from_managed_forest",                              \
+            "units": "kg m-2 s-1",                                                                     \
+            "cell_methods": "area: mean where land time: mean",                                        \
+            "cell_measures": "area: areacella",                                                        \
+            "long_name": "Total amount of stem carbon harvested from managed forest",                  \
+            "comment": "Total carbon harvested from stems of standing trees in a managed forest",      \
+            "dimensions": "longitude latitude time",                                                   \
+            "out_name": "fStemManaf",                                                                  \
+            "type": "real",                                                                            \
+            "positive": "up",                                                                          \
+            "valid_min": "",                                                                           \
+            "valid_max": "",                                                                           \
+            "ok_min_mean_abs": "",                                                                     \
+            "ok_max_mean_abs": ""                                                                      \
         },
   ' ${table_file_Eyr}
 
   # Remove the trailing spaces of the inserted block above:
-  sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_LPJGyr}
+ #sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_LPJGyr}
   sed -i -e 's/\s*$//g' -e 's/,$/, /g' ${table_file_Eyr}
 
   cd -
@@ -159,7 +198,7 @@ if [ "$#" -eq 0 ]; then
   echo " Running:"
   echo "  $0"
   echo " has adjusted the file:"
-  echo "  ${table_path}/${table_file_LPJGyr}"
+ #echo "  ${table_path}/${table_file_LPJGyr}"
   echo "  ${table_path}/${table_file_Eyr}"
   echo " which is part of the nested CMOR Table repository. View the diff by running:"
   echo "  cd ${table_path}; git diff; cd -"
