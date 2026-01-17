7b716e2c585d6230d6adbc0c9921ebb56b5146ce
treerink <thomas.reerink.knmi@gmail.com>
2026-01-17 01:10:09 +0100

    Add experiment overview. Add run examples for prio XML building. Add to-do list.

--- ece2cmor3/scripts/cmip7/start-cmip7.sh
+++ ece2cmor3/scripts/cmip7/start-cmip7.sh
@@ -95,3 +95,141 @@
           request-overview-cmip6-pextra-all-ECE3-CC.xml                  \
           request-overview-cmip6-pextra-all-ECE3-CC-neat-formatted.xml   \
           bup/cmip7-genecec-files/v03
+
+
+
+# Not longer necessary:
+#  # The command below takes rather long to created the log file:
+#  ./cmip7-request.py --all_opportunities --priority_cutoff high v1.2.2.3 cmip7-v1.2.2.3-request-all-experiments.json > cmip7-v1.2.2.3-request-all-experiments.log
+#  # Create a list of all Core & High prio variables for all experiments:
+#  grep -v -e TOTAL -e 'CMIP7' -e 'For data request' -e 'Wrote requested' cmip7-v1.2.2.3-request-all-experiments.log | sort | uniq | sed '1d' > cmip7-v1.2.2.3-request-all-experiments-uniq-varlist.txt
+
+ # Producing the core variable set
+ ./cmip7-request.py --all_opportunities --experiments historical                                                                               --priority_cutoff core v1.2.2.3
+
+ # A quick test example:
+ ./cmip7-request.py --all_opportunities --experiments tipmip-provisional-esm-up2p0-gwl4p0-50y-dn2p0-gwl2p0,tipmip-provisional-esm-up2p0-gwl5p0 --priority_cutoff low v1.2.2.3
+ ./cmip7-request.py --all_opportunities --experiments piControl                                                                                --priority_cutoff low v1.2.2.3
+ ./cmip7-request.py --all_opportunities --experiments piControl,historical                                                                     --priority_cutoff low v1.2.2.3
+
+ # Include all experiments with all priority variables (creating the XML file with CMIP7 variables including their priority as attribute:
+ ./cmip7-request.py --all_opportunities --priority_cutoff low v1.2.2.3 > cmip7-request-v1.2.2.3-all-low.log
+
+# To do:
+#
+# - use core prio only, see what is missing
+# - dir with xml files, archive under repository
+# - write XML files for identification selections
+# - Use request overview neat formatted file to loop over the identified variables and add / adjust / compare the ECE field_def file
+# - manage the 660 unidentified CMIP7 requested variables: ordened by realm and then alphabetical per variable name (variables on various tables are thus bundled)
+# - lpjg variables via .ins files
+
+# - DRS format and with help of CMIP7 attributes a cmor-metadata-fixer for CMIP6 => CMIP7 possible?
+
+# We have:
+#
+# - a list of 101 unique ECE3-CMIP6 variables which were identified for ECE3 but which are not requested by CMIP7, if all table - var combinations are taken into account this are 238 combinations
+# - a list of 660 (ECE4) CMIP7 unidentified variables, seperated in realm groups
+# - a list of 531 unique variables which are identified in the ECE3 - CMIP6 framework, which need to be incorporated into the ECE4 field_def file.
+#   these are grouped by model-component (and preferences) and thereby also by realm
+
+
+
+# For data request version v1.2.2.3, number of requested variables found by experiment:
+#   1pctCO2                                                 : Core=  131, High=  915, Medium=  135, Low=   95, TOTAL= 1276
+#   1pctCO2-bgc                                             : Core=  131, High=  809, Medium=  186, Low=   79, TOTAL= 1205
+#   1pctCO2-rad                                             : Core=  131, High=  809, Medium=  186, Low=   79, TOTAL= 1205
+#   abrupt-0p5CO2                                           : Core=  131, High=  954, Medium=  154, Low=   88, TOTAL= 1327
+#   abrupt-127k                                             : Core=  131, High=  873, Medium=  154, Low=   70, TOTAL= 1228
+#   abrupt-2xCO2                                            : Core=  131, High=  954, Medium=  154, Low=   88, TOTAL= 1327
+#   abrupt-4xCO2                                            : Core=  131, High=  915, Medium=  135, Low=   95, TOTAL= 1276
+#   amip                                                    : Core=  131, High=  978, Medium=  206, Low=   96, TOTAL= 1411
+#   amip-irr                                                : Core=  131, High=    6, Medium=   11, Low=   22, TOTAL=  170
+#   amip-m4K                                                : Core=  131, High=  263, Medium=    0, Low=   52, TOTAL=  446
+#   amip-noirr                                              : Core=  131, High=    6, Medium=   11, Low=   22, TOTAL=  170
+#   amip-p4K-SST-rad                                        : Core=  131, High=  263, Medium=    0, Low=   52, TOTAL=  446
+#   amip-p4K-SST-turb                                       : Core=  131, High=  263, Medium=    0, Low=   52, TOTAL=  446
+#   amip-p4k                                                : Core=  131, High= 1051, Medium=  214, Low=   97, TOTAL= 1493
+#   amip-piForcing                                          : Core=  131, High=  887, Medium=  184, Low=   97, TOTAL= 1299
+#   dcppA-assim                                             : Core=  131, High=   26, Medium=   96, Low=    0, TOTAL=  253
+#   dcppA-hindcast                                          : Core=  131, High=   26, Medium=   96, Low=    0, TOTAL=  253
+#   dcppB-forecast                                          : Core=  131, High=   26, Medium=   96, Low=    0, TOTAL=  253
+#   dcppB-forecast-cmip6                                    : Core=  131, High=  852, Medium=  270, Low=   87, TOTAL= 1340
+#   esm-flat10                                              : Core=  131, High=  795, Medium=  173, Low=   70, TOTAL= 1169
+#   esm-flat10-cdr                                          : Core=  131, High=  795, Medium=  173, Low=   70, TOTAL= 1169
+#   esm-flat10-zec                                          : Core=  131, High=  795, Medium=  173, Low=   70, TOTAL= 1169
+#   esm-hist                                                : Core=  131, High= 1130, Medium=  481, Low=  112, TOTAL= 1854
+#   esm-piControl                                           : Core=  131, High= 1035, Medium=  351, Low=  101, TOTAL= 1618
+#   esm-s7h-noFireChange                                    : Core=  131, High=   82, Medium=    0, Low=    0, TOTAL=  213
+#   esm-scen7-h                                             : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   esm-scen7-h-AQ                                          : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   esm-scen7-h-Aer                                         : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   esm-scen7-h-ext                                         : Core=  131, High=  372, Medium=  280, Low=   49, TOTAL=  832
+#   esm-scen7-hl                                            : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   esm-scen7-hl-ext                                        : Core=  131, High=  289, Medium=  209, Low=   47, TOTAL=  676
+#   esm-scen7-l                                             : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   esm-scen7-l-ext                                         : Core=  131, High=  362, Medium=  152, Low=   49, TOTAL=  694
+#   esm-scen7-ln                                            : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   esm-scen7-ln-ext                                        : Core=  131, High=  279, Medium=   81, Low=   47, TOTAL=  538
+#   esm-scen7-m                                             : Core=  131, High= 1070, Medium=  483, Low=   99, TOTAL= 1783
+#   esm-scen7-m-ext                                         : Core=  131, High=  372, Medium=  280, Low=   49, TOTAL=  832
+#   esm-scen7-ml                                            : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   esm-scen7-ml-ext                                        : Core=  131, High=  325, Medium=  231, Low=   47, TOTAL=  734
+#   esm-scen7-vl                                            : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   esm-scen7-vl-AQ                                         : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   esm-scen7-vl-Aer                                        : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   esm-scen7-vl-ext                                        : Core=  131, High=  289, Medium=  209, Low=   47, TOTAL=  676
+#   g7-1p5K-sai                                             : Core=  131, High=  855, Medium=  212, Low=   71, TOTAL= 1269
+#   highres-future-scen7-m                                  : Core=  131, High=   68, Medium=   94, Low=   24, TOTAL=  317
+#   highresSST-p2k-pat                                      : Core=  131, High=   68, Medium=   94, Low=   24, TOTAL=  317
+#   highresSST-p4k-pat                                      : Core=  131, High=   68, Medium=   94, Low=   24, TOTAL=  317
+#   hist-1950                                               : Core=  131, High=   68, Medium=   94, Low=   24, TOTAL=  317
+#   hist-GHG                                                : Core=  131, High=  864, Medium=  232, Low=   79, TOTAL= 1306
+#   hist-aer                                                : Core=  131, High=  864, Medium=  232, Low=   79, TOTAL= 1306
+#   hist-irr                                                : Core=  131, High=    6, Medium=   11, Low=   22, TOTAL=  170
+#   hist-nat                                                : Core=  131, High=  864, Medium=  232, Low=   79, TOTAL= 1306
+#   hist-noFire                                             : Core=  131, High=   82, Medium=    0, Low=    0, TOTAL=  213
+#   hist-noirr                                              : Core=  131, High=    6, Medium=   11, Low=   22, TOTAL=  170
+#   hist-piAQ                                               : Core=  131, High=  805, Medium=  184, Low=   79, TOTAL= 1199
+#   hist-piAer                                              : Core=  131, High=  805, Medium=  184, Low=   79, TOTAL= 1199
+#   historical                                              : Core=  131, High= 1130, Medium=  481, Low=  112, TOTAL= 1854
+#   land-hist                                               : Core=  131, High=  834, Medium=  241, Low=   71, TOTAL= 1277
+#   piClim-4xCO2                                            : Core=  131, High=  822, Medium=  117, Low=   90, TOTAL= 1160
+#   piClim-CH4                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-N2O                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-NOX                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-ODS                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-SO2                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-aer                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   piClim-anthro                                           : Core=  131, High=  839, Medium=  122, Low=   90, TOTAL= 1182
+#   piClim-control                                          : Core=  131, High=  822, Medium=  117, Low=   90, TOTAL= 1160
+#   piClim-histaer                                          : Core=  131, High=  795, Medium=  173, Low=   70, TOTAL= 1169
+#   piClim-histall                                          : Core=  131, High=  795, Medium=  173, Low=   70, TOTAL= 1169
+#   piControl                                               : Core=  131, High= 1035, Medium=  351, Low=  101, TOTAL= 1618
+#   scen7-h                                                 : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   scen7-h-AQ                                              : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   scen7-h-Aer                                             : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   scen7-h-ext                                             : Core=  131, High=  389, Medium=  281, Low=   49, TOTAL=  850
+#   scen7-hl                                                : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   scen7-hl-ext                                            : Core=  131, High=  307, Medium=  211, Low=   47, TOTAL=  696
+#   scen7-l                                                 : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   scen7-l-ext                                             : Core=  131, High=  379, Medium=  153, Low=   49, TOTAL=  712
+#   scen7-ln                                                : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   scen7-ln-ext                                            : Core=  131, High=  297, Medium=   83, Low=   47, TOTAL=  558
+#   scen7-m                                                 : Core=  131, High= 1070, Medium=  483, Low=   99, TOTAL= 1783
+#   scen7-m-ext                                             : Core=  131, High=  389, Medium=  281, Low=   49, TOTAL=  850
+#   scen7-ml                                                : Core=  131, High= 1059, Medium=  460, Low=  102, TOTAL= 1752
+#   scen7-ml-ext                                            : Core=  131, High=  343, Medium=  233, Low=   47, TOTAL=  754
+#   scen7-vl                                                : Core=  131, High= 1027, Medium=  438, Low=  102, TOTAL= 1698
+#   scen7-vl-AQ                                             : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   scen7-vl-Aer                                            : Core=  131, High=  799, Medium=  177, Low=   73, TOTAL= 1180
+#   scen7-vl-ext                                            : Core=  131, High=  307, Medium=  211, Low=   47, TOTAL=  696
+#   tipmip-provisional-esm-up2p0                            : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl1p5                     : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl2p0                     : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl2p0-50y-dn2p0           : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl3p0                     : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl4p0                     : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl4p0-50y-dn2p0           : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl4p0-50y-dn2p0-gwl2p0    : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
+#   tipmip-provisional-esm-up2p0-gwl5p0                     : Core=  131, High=   16, Medium=   36, Low=    0, TOTAL=  183
