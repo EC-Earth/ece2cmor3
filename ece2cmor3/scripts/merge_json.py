d4aca9754cafcafcbd14d3f98512618ca14a746a
treerink <thomas.reerink.knmi@gmail.com>
2026-02-11 14:52:30 +0100

    Add 3rd option: output file name. Add examples. Add description.

--- ece2cmor3/scripts/merge_json.py
+++ ece2cmor3/scripts/merge_json.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # Thomas Reerink
 #
-# This script converts a component json file produced by genecec including the preferences
-# into a flat json without the component structue (ifs, nemo, lpjg, tm5 & co2box) such that it can be
-# read with: checkvars -v --asciionly --drq flat-json-file.json --output request-overview
-#
-# Note that when a flat json file is given (instead of a component json file) the produced json
-# file will be just equal to the input flat json file.
+# This script combines and merges two json files which can have nested items. This script is aimed to combine
+# two datarequest varlist json files in order to create a joined request from two seperate requests.
 #
 # Run this script without arguments for examples how to call this script.
 #
@@ -39,10 +35,16 @@ def merge(source, destination):
 # Main program
 def main():
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 3 or len(sys.argv) == 4:
 
-       input_json_file_1 = sys.argv[1]                                                           # Reading the 1st data request file name from the argument line
-       input_json_file_2 = sys.argv[2]                                                           # Reading the 2nd data request file name from the argument line
+       input_json_file_1 = sys.argv[1]                                                           # The name of the 1st data request file name from the argument line
+       input_json_file_2 = sys.argv[2]                                                           # The name of the 2nd data request file name from the argument line
+
+       if len(sys.argv) == 4:
+        output_flat_json_file = sys.argv[3]                                                      # The name of the combined-merged output file if specified
+       else:
+        output_flat_json_file = os.path.basename(input_json_file_1).replace('.json','-merged-with-' + os.path.basename(input_json_file_2))
+
        if os.path.isfile(input_json_file_1) == False:                                            # Checking if the data request file exists
         print(error_message, ' The data request file ', input_json_file_1, ' does not exist.\n')
         sys.exit()
@@ -57,10 +59,10 @@ def main():
            data_request_2 = json.load(json_file)
        json_file.close()
 
-       output_flat_json_file = os.path.basename(input_json_file_1).replace('.json','-merged-with-' + os.path.basename(input_json_file_2))
 
        print('\n Running {:} with:\n  {:} {:} {:}\n'.format(os.path.basename(sys.argv[0]), os.path.basename(sys.argv[0]), sys.argv[1], sys.argv[2]))
 
+       # Recursive merge of netsed dictionaries:
        merge(data_request_1, data_request_2)
 
        data_request_merged = data_request_2
@@ -77,12 +79,15 @@ def main():
        print()
 
     else:
-       file_1 = 'bup/optimesm/optimesm-v02/optimesm-request-EC-EARTH-ESM-1-varlist.json'
-       file_2 = 'cmip7/bup/output-control-files-ECE3-ESM-1-CMIP7-esm-hist-high/cmip7/esm-hist-high-EC-Earth3-ESM-1/component-request-cmip7-esm-hist-high-EC-Earth3-ESM-1.json'
-       
+       file_1      = 'bup/optimesm/optimesm-v03/optimesm-request-EC-EARTH-ESM-1-varlist.json'
+       file_2_core = 'cmip7/bup/output-control-files-ECE3-ESM-1-CMIP7-esm-hist-core-v03/cmip7/esm-hist-core-EC-Earth3-ESM-1/component-request-cmip7-esm-hist-core-EC-Earth3-ESM-1.json'
+       file_2_high = 'cmip7/bup/output-control-files-ECE3-ESM-1-CMIP7-esm-hist-high-v03/cmip7/esm-hist-high-EC-Earth3-ESM-1/component-request-cmip7-esm-hist-high-EC-Earth3-ESM-1.json'
+       file_3_core = 'combinded-optimesm-cmip7-core-request.json'
+       file_3_high = 'combinded-optimesm-cmip7-high-request.json'
        print()
-       print('  This scripts requires two arguments, two json data request files, e.g.:')
-       print('  ./{} {} {}'.format(os.path.basename(sys.argv[0]), file_1, file_2))
+       print('  This scripts requires two or three arguments, two json data request files, e.g.:')
+       print('  ./{} {} {} {}'.format(os.path.basename(sys.argv[0]), file_1, file_2_core, file_3_core))
+       print('  ./{} {} {} {}'.format(os.path.basename(sys.argv[0]), file_1, file_2_high, file_3_high))
        print()
 
 if __name__ == "__main__":
