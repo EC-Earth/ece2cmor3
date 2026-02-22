e983f1e74df84508d6959c0bf5a3d11852652970
treerink <thomas.reerink.knmi@gmail.com>
2026-02-22 14:40:33 +0100

    Add surface ocnBgchem variables #866.

--- ece2cmor3/scripts/cmip7/recmorise-cmip6-to-cmip7.py
+++ ece2cmor3/scripts/cmip7/recmorise-cmip6-to-cmip7.py
@@ -282,7 +282,7 @@ def main():
 
     # Define the CMOR variable object
 
-    if cmip7_realm in ['ocean', 'seaIce']:
+    if cmip7_realm in ['ocean', 'seaIce', 'ocnBgchem']:
      orca_grid_case = True
     else:
      orca_grid_case = False
