b9a22ba91bfd5bc6d29229f24304054762b209a0
Klaus Wyser <klaus.wyser@smhi.se>
2026-03-10 12:37:14 +0100

    delete more erroneously committed files

--- ece2cmor3/for_turkije.sh
+++
@@ -1,1 +0,0 @@
-ece2cmor.py --exp t621 --varlist varlist-turkije.json --meta --ifs --odir ~/r27/for_turkije/export-3 --npp 12 --tmpdir $SNIC_TMP ~/crescendo/ece-run/t621/output/ifs/166 >& logfile-turkije
