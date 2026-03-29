ec606b6f503185d41ab204c233802faec257bb67
treerink <thomas.reerink.knmi@gmail.com>
2026-03-29 16:10:28 +0200

    Add argument to script: CMIP7 priority #863.

--- ece2cmor3/scripts/combine-optimesm-and-cmip7-requests.sh
+++ ece2cmor3/scripts/combine-optimesm-and-cmip7-requests.sh
@@ -1,58 +1,72 @@
 #!/usr/bin/env bash
 # Thomas Reerink
 #
-# This script creates the output-control-files based on the combined rdata requests from OptimESM and CMIP7 (core & high) esm-hist
+# This script creates the output-control-files based on the combined data requests from OptimESM and CMIP7 (core or high) esm-hist for EC-Earth3-ESM-1.
 #
-# This script requires one argument: a version label
+# This script requires two arguments: 1. The CMIP7 priority level 2. A version label
 #
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
 
- version=$1
+ priority=$1
+ version=$2
 
- mip=CMIP
- experiment=esm-hist
- 
- cmip7_dir=cmip7
- archive_dir=archive
- optimesm_dir=cmip7/${archive_dir}/optimesm-${version}
+ if [ $1 == 'core' ] || [ $1 == 'high' ] || [ $1 == 'medium' ] || [ $1 == 'low' ]; then
+  mip=CMIP
+  experiment=esm-hist
 
- # Produce component varlist request files and move them to an archive:
- ./add-optimesm-variables.sh >& add-optimesm-variables.log    # This adds the 6hrLev zg for the high request below
- cd ${cmip7_dir}
- mkdir -p ${archive_dir}
- ./genecec-cmip7-wrapper.sh core ${experiment} EC-Earth3-ESM-1;  mv cmip7-output-control-files ${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-core-${version}/
- ./genecec-cmip7-wrapper.sh high ${experiment} EC-Earth3-ESM-1;  mv cmip7-output-control-files ${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-high-${version}/
+  cmip7_dir=cmip7
+  archive_dir=archive
+  optimesm_dir=cmip7/${archive_dir}/optimesm-${version}
 
- # This includes the production of the cmip6Plus varlists (which are not used for this case):
- cd ../
- ./genecec-for-individual-experiments.sh ../resources/miscellaneous-data-requests/optimesm-request/optimesm-request-EC-EARTH-ESM-1-varlist.json ${mip} ${experiment} EC-EARTH-ESM-1 ${optimesm_dir}/  &> genecec-for-individual-experiments.log
- mv -f genecec-for-individual-experiments.log ${optimesm_dir}/
+  # Produce component varlist request files and move them to an archive:
+  ./add-optimesm-variables.sh >& add-optimesm-variables.log    # This adds the 6hrLev zg for the high request below
+  cd ${cmip7_dir}
+  mkdir -p ${archive_dir}
+  ./genecec-cmip7-wrapper.sh ${priority} ${experiment} EC-Earth3-ESM-1
+  mv cmip7-output-control-files ${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-${priority}-${version}/
 
- # Combine and merge the OptimESM data request files and the CMIP7 request files:
- ./combine-and-merge-json-request-files.py ${optimesm_dir}/optimesm-request-EC-EARTH-ESM-1-varlist.json cmip7/${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-core-${version}/${experiment}-core-EC-Earth3-ESM-1/component-request-cmip7-${experiment}-core-EC-Earth3-ESM-1.json combined-optimesm-cmip7-core-request-EC-EARTH-ESM-1-varlist.json
- ./combine-and-merge-json-request-files.py ${optimesm_dir}/optimesm-request-EC-EARTH-ESM-1-varlist.json cmip7/${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-high-${version}/${experiment}-high-EC-Earth3-ESM-1/component-request-cmip7-${experiment}-high-EC-Earth3-ESM-1.json combined-optimesm-cmip7-high-request-EC-EARTH-ESM-1-varlist.json
+  # This includes the production of the cmip6Plus varlists (which are not used for this case):
+  cd ../
+  ./genecec-for-individual-experiments.sh ../resources/miscellaneous-data-requests/optimesm-request/optimesm-request-EC-EARTH-ESM-1-varlist.json ${mip} ${experiment} EC-EARTH-ESM-1 ${optimesm_dir}/  &> genecec-for-individual-experiments.log
+  mv -f genecec-for-individual-experiments.log ${optimesm_dir}/
 
- # Use the combined optimesm and CMIP7 (core or high) request to generate the combined output-control-files:
- ./genecec-for-individual-experiments.sh combined-optimesm-cmip7-core-request-EC-EARTH-ESM-1-varlist.json                                       ${mip} ${experiment} EC-EARTH-ESM-1 cmip7/${archive_dir}/optimesm-core-combined-${version}/ &> genecec-for-individual-experiments-combined-core.log
- ./genecec-for-individual-experiments.sh combined-optimesm-cmip7-high-request-EC-EARTH-ESM-1-varlist.json                                       ${mip} ${experiment} EC-EARTH-ESM-1 cmip7/${archive_dir}/optimesm-high-combined-${version}/ &> genecec-for-individual-experiments-combined-high.log
- mv -f genecec-for-individual-experiments-combined-core.log cmip7/${archive_dir}/optimesm-core-combined-${version}/
- mv -f genecec-for-individual-experiments-combined-high.log cmip7/${archive_dir}/optimesm-high-combined-${version}/
+  # Combine and merge the OptimESM data request files and the CMIP7 request files:
+  ./combine-and-merge-json-request-files.py ${optimesm_dir}/optimesm-request-EC-EARTH-ESM-1-varlist.json cmip7/${archive_dir}/cmip7-output-control-files-EC-Earth3-ESM-1-CMIP7-${experiment}-${priority}-${version}/${experiment}-${priority}-EC-Earth3-ESM-1/component-request-cmip7-${experiment}-${priority}-EC-Earth3-ESM-1.json combined-optimesm-cmip7-${priority}-request-EC-EARTH-ESM-1-varlist.json
 
- ./add-Oday-zos-for-OptimESM-to-xml.sh cmip7/${archive_dir}/optimesm-core-combined-${version}/file_def_nemo-opa.xml
- ./add-Oday-zos-for-OptimESM-to-xml.sh cmip7/${archive_dir}/optimesm-high-combined-${version}/file_def_nemo-opa.xml
+  # Use the combined optimesm and CMIP7 request to generate the combined output-control-files:
+  ./genecec-for-individual-experiments.sh combined-optimesm-cmip7-${priority}-request-EC-EARTH-ESM-1-varlist.json ${mip} ${experiment} EC-EARTH-ESM-1 cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/ &> genecec-for-individual-experiments-combined-${priority}.log
+  mv -f genecec-for-individual-experiments-combined-${priority}.log cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/
 
- ./revert-nested-cmor-table-branch.sh
+  ./add-Oday-zos-for-OptimESM-to-xml.sh cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/file_def_nemo-opa.xml
 
- echo
- echo " Finished, the result can be found here:"
- echo "  cmip7/${archive_dir}/optimesm-core-combined-${version}/"
- echo "  cmip7/${archive_dir}/optimesm-high-combined-${version}/"
- echo
+  ./revert-nested-cmor-table-branch.sh
+
+  echo
+  echo " Finished, the result can be found here:"
+  echo "  cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/"
+  echo
+
+ else
+  echo
+  echo " Error: the value of the first argument is wrong."
+  echo
+  echo " This scripts requires two argument: There are only four options for the first argument:"
+  echo "  $0 core   ${version}"
+  echo "  $0 high   ${version}"
+  echo "  $0 medium ${version}"
+  echo "  $0 low    ${version}"
+  echo
+ fi
 
 else
  echo
- echo " This scripts requires one argument:"
- echo "  $0 v01"
+ echo " This scripts requires two arguments:"
+ echo "  1. The CMIP7 priority level [core, high, medium, low]"
+ echo "  2. A version label"
+ echo
+ echo " For instance:"
+ echo "  $0 core v01"
+ echo "  $0 high v01"
  echo
 fi
