4c15cbc98ab4f2f18a6d6b770f8eee177ca5ea6d
treerink <thomas.reerink.knmi@gmail.com>
2026-03-29 17:44:24 +0200

    Define a few more variables. #863.

--- ece2cmor3/scripts/combine-optimesm-and-cmip7-requests.sh
+++ ece2cmor3/scripts/combine-optimesm-and-cmip7-requests.sh
@@ -14,33 +14,34 @@ if [ "$#" -eq 2 ]; then
  if [ $1 == 'core' ] || [ $1 == 'high' ] || [ $1 == 'medium' ] || [ $1 == 'low' ]; then
   mip=CMIP
   experiment=esm-hist
-  ECE3=EC-Earth3-ESM-1
-
+  ECE3_name=EC-Earth3-ESM-1
+  ECE_model=EC-EARTH-ESM-1
   cmip7_dir=cmip7
   archive_dir=archive
   optimesm_dir=cmip7/${archive_dir}/optimesm-${version}
-  ece3_cmip7_dir=${archive_dir}/cmip7-${priority}-${experiment}-${ECE3}-${version}/
+  ece3_cmip7_dir=${archive_dir}/cmip7-${priority}-${experiment}-${ECE3_name}-${version}/
 
-  optimesm_request=../resources/miscellaneous-data-requests/optimesm-request/optimesm-request-EC-EARTH-ESM-1-varlist.json
+  optimesm_request=../resources/miscellaneous-data-requests/optimesm-request/optimesm-request-${ECE_model}-varlist.json
+  combined_request=combined-optimesm-cmip7-${priority}-request-${ECE_model}-varlist.json
 
   # Produce component varlist request files and move them to an archive:
   ./add-optimesm-variables.sh >& add-optimesm-variables.log    # This adds the 6hrLev zg for the high request below
   cd ${cmip7_dir}
   mkdir -p ${archive_dir}
-  ./genecec-cmip7-wrapper.sh ${priority} ${experiment} ${ECE3}
-  mv -f cmip7-output-control-files/log-files cmip7-output-control-files/${experiment}-${priority}-${ECE3}/
-  mv -f cmip7-output-control-files/${experiment}-${priority}-${ECE3}/ ${ece3_cmip7_dir}/
+  ./genecec-cmip7-wrapper.sh ${priority} ${experiment} ${ECE3_name}
+  mv -f cmip7-output-control-files/log-files cmip7-output-control-files/${experiment}-${priority}-${ECE3_name}/
+  mv -f cmip7-output-control-files/${experiment}-${priority}-${ECE3_name}/ ${ece3_cmip7_dir}/
 
   # This includes the production of the cmip6Plus varlists (which are not used for this case):
   cd ../
-  ./genecec-for-individual-experiments.sh ${optimesm_request} ${mip} ${experiment} EC-EARTH-ESM-1 ${optimesm_dir}/  &> genecec-for-individual-experiments.log
+  ./genecec-for-individual-experiments.sh ${optimesm_request} ${mip} ${experiment} ${ECE_model} ${optimesm_dir}/  &> genecec-for-individual-experiments.log
   mv -f genecec-for-individual-experiments.log ${optimesm_dir}/
 
   # Combine and merge the OptimESM data request files and the CMIP7 request files:
-  ./combine-and-merge-json-request-files.py ${optimesm_dir}/optimesm-request-EC-EARTH-ESM-1-varlist.json cmip7/${ece3_cmip7_dir}/component-request-cmip7-${experiment}-${priority}-${ECE3}.json combined-optimesm-cmip7-${priority}-request-EC-EARTH-ESM-1-varlist.json
+  ./combine-and-merge-json-request-files.py ${optimesm_dir}/${optimesm_request##*/} cmip7/${ece3_cmip7_dir}/component-request-cmip7-${experiment}-${priority}-${ECE3_name}.json ${combined_request}
 
   # Use the combined optimesm and CMIP7 request to generate the combined output-control-files:
-  ./genecec-for-individual-experiments.sh combined-optimesm-cmip7-${priority}-request-EC-EARTH-ESM-1-varlist.json ${mip} ${experiment} EC-EARTH-ESM-1 cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/ &> genecec-for-individual-experiments-combined-${priority}.log
+  ./genecec-for-individual-experiments.sh ${combined_request} ${mip} ${experiment} ${ECE_model} cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/ &> genecec-for-individual-experiments-combined-${priority}.log
   mv -f genecec-for-individual-experiments-combined-${priority}.log cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/
 
   ./add-Oday-zos-for-OptimESM-to-xml.sh cmip7/${archive_dir}/optimesm-${priority}-combined-${version}/file_def_nemo-opa.xml
@@ -48,7 +49,7 @@ if [ "$#" -eq 2 ]; then
   ./revert-nested-cmor-table-branch.sh
 
   # Clean:
-  rm -f combined-optimesm-cmip7-${priority}-request-EC-EARTH-ESM-1-varlist.json add-optimesm-variables.log
+  rm -f ${combined_request} add-optimesm-variables.log
 
   echo
   echo " Finished, the result can be found here:"
