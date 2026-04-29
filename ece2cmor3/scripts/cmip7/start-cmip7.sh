17c688528f87691e4e98a92489bb587925b75f98
treerink <thomas.reerink.knmi@gmail.com>
2026-04-29 13:24:54 +0200

    Order files on most usage #883.

--- ece2cmor3/scripts/cmip7/start-cmip7.sh
+++ ece2cmor3/scripts/cmip7/start-cmip7.sh
@@ -176,14 +176,11 @@
 
 
 # Opening freqeuntly used files:
-#  n *.py config-* *.sh
-#  n create-basic-ec-earth-cmip6-nemo-namelist.py config-create-basic-ec-earth-cmip6-nemo-namelist create-basic-ec-earth-cmip6-nemo-namelist.log ../resources/xios-nemo-file_def-files/basic-* ~/ec-earth/ecearth3/runtime/classic/ctrl/ping_* ~/ec-earth/ecearth3/runtime/classic/ctrl/field_def_nemo-*
-#  n ~/cmorize/control-output-files/output-control-files-v462/cmip6-pextra/test-all-ece-mip-variables/*
-#  n xml-files/genecec-cmip7/*.xml xml-files/genecec-cmip7/ping-files/ec-earth-ping-neat-formatted.xml xml-files/genecec-cmip7/ec-earth-definition/ec-earth-definition-neat-formatted.xml xml-files/genecec-cmip7/ec-earth-definition/ec-earth-definition-inherited-neat-formatted.xml xml-files/genecec-cmip7/identify-ece4-cmip7/cmip7-request-v1.2.2.3-all-full-priority.xml xml-files/genecec-cmip7/identify-ece4-cmip7/cmip7-request-v1.2.2.3-all-full-*identified-freq*prio.xml
-#  n ~/ec-earth/ecearth4/scripts/runtime/templates/xios/field_def_oifs_*
-
-# cd /home/reerink/ec-earth/ecearth4/scripts/runtime/templates/xios
-# n axis_def_oifs.xml.j2 domain_def_oifs.xml.j2 grid_def_oifs.xml.j2 context_oifs.xml.j2 field_def_oifs_*j2 file_def_oifs_*
+   n *.py config-* *.sh
+   n xml-files/genecec-cmip7/*.xml xml-files/genecec-cmip7/ping-files/ec-earth-ping-neat-formatted.xml xml-files/genecec-cmip7/ec-earth-definition/ec-earth-definition-neat-formatted.xml xml-files/genecec-cmip7/ec-earth-definition/ec-earth-definition-inherited-neat-formatted.xml xml-files/genecec-cmip7/identify-ece4-cmip7/cmip7-request-v1.2.2.3-all-full-priority.xml xml-files/genecec-cmip7/identify-ece4-cmip7/cmip7-request-v1.2.2.3-all-full-*identified-freq*prio.xml
+   n ~/ec-earth/ecearth4/scripts/runtime/templates/xios/*oifs*
+   n create-basic-ec-earth-cmip6-nemo-namelist.py config-create-basic-ec-earth-cmip6-nemo-namelist create-basic-ec-earth-cmip6-nemo-namelist.log ../resources/xios-nemo-file_def-files/basic-* ~/ec-earth/ecearth3/runtime/classic/ctrl/ping_* ~/ec-earth/ecearth3/runtime/classic/ctrl/field_def_nemo-*
+   n ~/cmorize/control-output-files/output-control-files-v462/cmip6-pextra/test-all-ece-mip-variables/*
 
 
 # To do:
