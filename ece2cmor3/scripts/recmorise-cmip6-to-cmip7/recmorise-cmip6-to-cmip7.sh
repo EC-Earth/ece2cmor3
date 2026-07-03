17f0aa6de6bd122024dd27b7f4a2add08f237329
treerink <thomas.reerink.knmi@gmail.com>
2026-07-03 13:51:52 +0200

    Add one arg to script: a config file #866.

--- ece2cmor3/scripts/recmorise-cmip6-to-cmip7/recmorise-cmip6-to-cmip7.sh
+++ ece2cmor3/scripts/recmorise-cmip6-to-cmip7/recmorise-cmip6-to-cmip7.sh
@@ -3,31 +3,47 @@
 # This script creates the run-recmorise-cmip6-to-cmip7.sh script which calls the recmorise-cmip6-to-cmip7.py
 # script for all CMIP6 variable combinations for a given CMIP6 top directory with CMIP6 cmorised data.
 #
-# This script requires no arguments.
+# This script requires one argument.
 
- config_file=config-file-recmorisation
+ if [ "$#" = 1 ]; then
+   config_file=$1
 
- # Catch all the path variables from the config file:
- ece3_cmip6_data_dir_root=`grep -e cmip6_input_dir_name ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- cmip6_source_id=`         grep -e cmip6_source_id      ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- institution_id=`          grep -e institution_id       ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- experiment_id=`           grep -e '^experiment_id'     ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- ripf_r=`                  grep -e ripf_r               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- ripf_i=`                  grep -e ripf_i               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- ripf_p=`                  grep -e ripf_p               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- ripf_f=`                  grep -e ripf_f               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
- # Replace the tilde:
- ece3_cmip6_data_dir_root="${ece3_cmip6_data_dir_root/#\~/$HOME}"
+   if [ ! -e ${config_file} ]; then
+    echo; echo " The file ${config_file} does not exist."; echo
+    exit
+   fi
 
- base_path=${ece3_cmip6_data_dir_root}CMIP6/CMIP/${institution_id}/${cmip6_source_id}/${experiment_id}/${ripf_r}${ripf_i}${ripf_p}${ripf_f}
+   # Catch all the path variables from the config file:
+   ece3_cmip6_data_dir_root=`grep -e cmip6_input_dir_name ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   cmip6_source_id=`         grep -e cmip6_source_id      ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   institution_id=`          grep -e institution_id       ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   experiment_id=`           grep -e '^experiment_id'     ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   ripf_r=`                  grep -e ripf_r               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   ripf_i=`                  grep -e ripf_i               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   ripf_p=`                  grep -e ripf_p               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   ripf_f=`                  grep -e ripf_f               ${config_file} | sed -e "s/.*= //" -e "s/'//g" -e 's/ .*//'`
+   # Replace the tilde:
+   ece3_cmip6_data_dir_root="${ece3_cmip6_data_dir_root/#\~/$HOME}"
 
- for j in {3hr,6hrPlev,Amon,day,Efx,Emon,Eyr,fx,LImon,Lmon,Oday,Ofx,Omon,SIday,SImon,}; do
-   for i in `/usr/bin/ls -1 ${base_path}/$j`; do
-     printf " ./recmorise-cmip6-to-cmip7.py  -r -c ${config_file} %-8s %-20s  &>> recmorise-cmip6-to-cmip7.log\n" $j ${i}
-   done
-   echo
- done > run-recmorise-cmip6-to-cmip7.sh
+   base_path=${ece3_cmip6_data_dir_root}CMIP6/CMIP/${institution_id}/${cmip6_source_id}/${experiment_id}/${ripf_r}${ripf_i}${ripf_p}${ripf_f}
 
- chmod uog+x run-recmorise-cmip6-to-cmip7.sh
+   for j in {3hr,6hrPlev,Amon,day,Efx,Emon,Eyr,fx,LImon,Lmon,Oday,Ofx,Omon,SIday,SImon,}; do
+     for i in `/usr/bin/ls -1 ${base_path}/$j`; do
+       printf " ./recmorise-cmip6-to-cmip7.py  -r -c ${config_file} %-8s %-20s  &>> recmorise-cmip6-to-cmip7.log\n" $j ${i}
+     done
+     echo
+   done > run-recmorise-cmip6-to-cmip7.sh
 
-#./run-recmorise-cmip6-to-cmip7.sh
+   chmod uog+x run-recmorise-cmip6-to-cmip7.sh
+
+  #./run-recmorise-cmip6-to-cmip7.sh
+
+ else
+  echo
+  echo " Illegal number of arguments: This submit script requires one argument:"
+  echo "  1. A config file"
+  echo
+  echo " For instance:"
+  echo "  $0 config-file-recmorisation"
+  echo
+ fi
