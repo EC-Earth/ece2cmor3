9976f13fa25d8512827c135547516e0181fcfb59
treerink <thomas.reerink.knmi@gmail.com>
2026-08-07 12:00:18 +0200

    Fix bug, remove obsolete line in cmip7-request.py #883.

--- ece2cmor3/scripts/cmip7/cmip7-request.py
+++ ece2cmor3/scripts/cmip7/cmip7-request.py
@@ -490,9 +490,6 @@ def main():
       for message in message_list_changed_priorities:
        log_file.write(message)
 
-
-    add_all_attributes = True
-
     value_list_with_priorities       = ["Core", "High", "Medium", "Low"]
     value_list_with_cmip6_tables     = ["fx", "Efx", "AERfx", "Ofx", "IfxAnt", "IfxGre", \
                                         "3hr", "E3hr", "CF3hr", "3hrPt", "E3hrPt", "6hrPlev", "6hrPlevPt", "6hrLev", \
